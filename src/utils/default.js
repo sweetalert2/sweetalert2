e1fcf9f134b5861e9fbaed89c127f7b8037a42a1
Sam Turrell <sam@samturrell.co.uk>
2016-05-29 19:28:51 +0100

    Code cleanup (#146)

---
+++ src/utils/default.js
@@ -0,0 +1,68 @@
+import { swalClasses, iconTypes } from './classes.js';
+
+export var defaultParams = {
+  title: '',
+  text: '',
+  html: '',
+  type: null,
+  animation: true,
+  allowOutsideClick: true,
+  allowEscapeKey: true,
+  showConfirmButton: true,
+  showCancelButton: false,
+  preConfirm: null,
+  confirmButtonText: 'OK',
+  confirmButtonColor: '#3085d6',
+  confirmButtonClass: null,
+  cancelButtonText: 'Cancel',
+  cancelButtonColor: '#aaa',
+  cancelButtonClass: null,
+  buttonsStyling: true,
+  reverseButtons: false,
+  showCloseButton: false,
+  imageUrl: null,
+  imageWidth: null,
+  imageHeight: null,
+  imageClass: null,
+  timer: null,
+  width: 500,
+  padding: 20,
+  background: '#fff',
+  input: null, // 'text' | 'email' | 'password' | 'select' | 'radio' | 'checkbox' | 'textarea'
+  inputPlaceholder: '',
+  inputValue: '',
+  inputOptions: {},
+  inputAutoTrim: true,
+  inputClass: null,
+  inputAttributes: {},
+  inputValidator: null
+};
+
+export var sweetHTML = '<div class="' + swalClasses.overlay + '" tabIndex="-1"></div>' +
+  '<div class="' + swalClasses.modal + '" style="display: none" tabIndex="-1">' +
+    '<div class="' + swalClasses.icon + ' ' + iconTypes.error + '">' +
+      '<span class="x-mark"><span class="line left"></span><span class="line right"></span></span>' +
+    '</div>' +
+    '<div class="' + swalClasses.icon + ' ' + iconTypes.question + '">?</div>' +
+    '<div class="' + swalClasses.icon + ' ' + iconTypes.warning + '">!</div>' +
+    '<div class="' + swalClasses.icon + ' ' + iconTypes.info + '">i</div>' +
+    '<div class="' + swalClasses.icon + ' ' + iconTypes.success + '">' +
+      '<span class="line tip"></span> <span class="line long"></span>' +
+      '<div class="placeholder"></div> <div class="fix"></div>' +
+    '</div>' +
+    '<img class="' + swalClasses.image + '">' +
+    '<h2></h2>' +
+    '<div class="' + swalClasses.content + '"></div>' +
+    '<input class="' + swalClasses.input + '">' +
+    '<select class="' + swalClasses.select + '"></select>' +
+    '<div class="' + swalClasses.radio + '"></div>' +
+    '<label for="' + swalClasses.checkbox + '" class="' + swalClasses.checkbox + '">' +
+      '<input type="checkbox" id="' + swalClasses.checkbox + '">' +
+    '</label>' +
+    '<textarea class="' + swalClasses.textarea + '"></textarea>' +
+    '<div class="' + swalClasses.validationerror + '"></div>' +
+    '<hr class="' + swalClasses.spacer + '">' +
+    '<button class="' + swalClasses.confirm + '">OK</button>' +
+    '<button class="' + swalClasses.cancel + '">Cancel</button>' +
+    '<span class="' + swalClasses.close + '">&times;</span>' +
+  '</div>';
---
+++ src/utils/utils.js
@@ -0,0 +1,32 @@
+export var extend = function(a, b) {
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) {
+      a[key] = b[key];
+    }
+  }
+
+  return a;
+};
+
+
+/*
+ * Set hover, active and focus-states for buttons (source: http://www.sitepoint.com/javascript-generate-lighter-darker-color)
+ */
+export var colorLuminance = function(hex, lum) {
+  // Validate hex string
+  hex = String(hex).replace(/[^0-9a-f]/gi, '');
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  lum = lum || 0;
+
+  // Convert to decimal and change luminosity
+  var rgb = '#';
+  for (var i = 0; i < 3; i++) {
+    var c = parseInt(hex.substr(i * 2, 2), 16);
+    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+    rgb += ('00' + c).substr(c.length);
+  }
+
+  return rgb;
+};

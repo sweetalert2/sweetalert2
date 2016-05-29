7f8b2d901d05414a3475f01c65d013bab0e9b575
Sam Turrell <sam@samturrell.co.uk>
2016-05-29 19:28:51 +0100

    Code cleanup (#146)

---
+++ src/utils/classes.js
@@ -0,0 +1,36 @@
+export var swalPrefix = 'swal2-';
+
+export var prefix = function(items) {
+  var result = {};
+  for (var i in items) {
+    result[items[i]] = swalPrefix + items[i];
+  }
+  return result;
+};
+
+
+export var swalClasses = prefix([
+  'container', 
+  'modal', 
+  'overlay', 
+  'close', 
+  'content', 
+  'spacer', 
+  'confirm',
+  'cancel', 
+  'icon', 
+  'image', 
+  'input', 
+  'select', 
+  'radio', 
+  'checkbox', 
+  'textarea',
+  'validationerror'
+]);
+export var iconTypes = prefix([
+  'success', 
+  'warning', 
+  'info', 
+  'question', 
+  'error'
+]);

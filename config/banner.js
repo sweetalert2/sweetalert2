fd26c1a6761f463b80edb8cf180b514824dfab72
Limon Monte <limonte@users.noreply.github.com>
2016-05-30 20:52:32 +0200

    Remove author from banner.

--- config/banner.js
+++ config/banner.js
@@ -4,6 +4,5 @@ var version = process.env.VERSION || pack.version;
 module.exports =
   '/*!\n' +
   ' * ' + pack.name + ' v' + version + '\n' +
-  ' * (c) ' + new Date().getFullYear() + ' ' + pack.author + '\n' +
   ' * Released under the ' + pack.license + ' License.\n' +
   ' */';

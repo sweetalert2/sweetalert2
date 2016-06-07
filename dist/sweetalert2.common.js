d9ce129cc3423d8b59d5f4afb2d35e6f4e13730d
Limon Monte <limonte@users.noreply.github.com>
2016-06-07 14:00:40 +0200

    v3.3.1

--- dist/sweetalert2.common.js
+++ dist/sweetalert2.common.js
@@ -1,5 +1,5 @@
 /*!
- * sweetalert2 v3.3.0
+ * sweetalert2 v3.3.1
  * Released under the MIT License.
  */
 'use strict';
@@ -338,12 +338,12 @@ var resetPrevState = function() {
   }
 };
 
-var params = extend({}, defaultParams);
+var modalParams = extend({}, defaultParams);
 
 /*
  * Set type, text and actions on modal
  */
-var setParameters = function() {
+var setParameters = function(params) {
   var i;
   var modal = getModal();
 
@@ -636,9 +636,9 @@ var setParameters = function() {
 /*
  * Animations
  */
-var openModal = function() {
+var openModal = function(animation) {
   var modal = getModal();
-  if (params.animation) {
+  if (animation) {
     fadeIn(getOverlay(), 10);
     addClass(modal, 'show-swal2');
     removeClass(modal, 'hide-swal2');
@@ -668,9 +668,12 @@ function modalDependant() {
     return false;
   }
 
+  var params;
+
   switch (typeof arguments[0]) {
 
     case 'string':
+      params = modalParams;
       params.title = arguments[0];
       params.text  = arguments[1] || '';
       params.type  = arguments[2] || '';
@@ -678,7 +681,7 @@ function modalDependant() {
       break;
 
     case 'object':
-      extend(params, arguments[0]);
+      params = extend(modalParams, arguments[0]);
       params.extraParams = arguments[0].extraParams;
 
       if (params.input === 'email' && params.inputValidator === null) {
@@ -702,9 +705,9 @@ function modalDependant() {
 
   }
 
-  setParameters();
+  setParameters(params);
   fixVerticalPosition();
-  openModal();
+  openModal(params.animation);
 
   // Modal interactions
   var modal = getModal();
@@ -1051,6 +1054,25 @@ function sweetAlert() {
 }
 
 /*
+ * Global function for chaining sweetAlert modals
+ */
+sweetAlert.queue = function(steps) {
+  return new Promise(function(resolve) {
+    (function step(i, callback) {
+      if (i < steps.length) {
+        sweetAlert(steps[i]).then(function(isConfirm) {
+          if (isConfirm) {
+            step(i+1, callback);
+          }
+        });
+      } else {
+        resolve();
+      }
+    })(0);
+  });
+};
+
+/*
  * Global function to close sweetAlert
  */
 sweetAlert.close = sweetAlert.closeModal = function() {
@@ -1166,17 +1188,17 @@ sweetAlert.setDefaults = function(userParams) {
     throw new Error('userParams has to be a object');
   }
 
-  extend(params, userParams);
+  extend(modalParams, userParams);
 };
 
 /**
  * Reset default params for each popup
  */
 sweetAlert.resetDefaults = function() {
-  params = defaultParams;
+  modalParams = defaultParams;
 };
 
-sweetAlert.version = '3.3.0';
+sweetAlert.version = '3.3.1';
 
 window.sweetAlert = window.swal = sweetAlert;
 

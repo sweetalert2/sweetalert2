9ef46b9dc03c320e5991890e7deaebf47b0ff40e
birjolaxew <johanringmann@gmail.com>
2016-09-15 23:07:43 +0200

    Dirty fix #272 and dirty fix #277

--- src/utils/default.js
+++ src/utils/default.js
@@ -46,8 +46,7 @@ export var defaultParams = {
   onClose: null
 };
 
-export var sweetHTML = '<div class="' + swalClasses.overlay + '" tabIndex="-1"></div>' +
-  '<div class="' + swalClasses.modal + '" style="display: none" tabIndex="-1">' +
+export var sweetHTML = '<div class="' + swalClasses.modal + '" style="display: none" tabIndex="-1">' +
     '<ul class="' + swalClasses.progresssteps + '"></ul>' +
     '<div class="' + swalClasses.icon + ' ' + iconTypes.error + '">' +
       '<span class="x-mark"><span class="line left"></span><span class="line right"></span></span>' +
@@ -75,3 +74,6 @@ export var sweetHTML = '<div class="' + swalClasses.overlay + '" tabIndex="-1"><
     '<button type="button" class="' + swalClasses.cancel + '">Cancel</button>' +
     '<span class="' + swalClasses.close + '">&times;</span>' +
   '</div>';
+export var sweetWrap = document.createElement('div');
+sweetWrap.className = swalClasses.container;
+sweetWrap.innerHTML = sweetHTML;
--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,24 +1,22 @@
 import { swalPrefix, swalClasses } from './classes.js';
+import { sweetWrap } from './default.js';
 
 // Remember state in cases where opening and handling a modal will fiddle with it.
 export var states = {
   previousWindowKeyDown: null,
-  previousActiveElement: null
+  previousActiveElement: null,
+  previousBodyPadding: null
 };
 
 /*
  * Manipulate DOM
  */
 export var elementByClass = function(className) {
-  return document.querySelector('.' + className);
+  return sweetWrap.querySelector('.' + className);
 };
 
 export var getModal = function() {
   return elementByClass(swalClasses.modal);
-};
-
-export var getOverlay = function() {
-  return elementByClass(swalClasses.overlay);
 };
 
 export var getIcons = function() {
@@ -255,14 +253,14 @@ export var addMediaQuery = function(content) {
   return mediaqueryId;
 };
 
-// Remove dynamically created media query
-export var removeMediaQuery = function(mediaqueryId) {
-  if (!mediaqueryId) {
-    return false;
-  }
-  var head = document.getElementsByTagName('head')[0];
-  var mediaquery = document.getElementById(mediaqueryId);
-  if (mediaquery) {
-    head.removeChild(mediaquery);
-  }
+// measure width of scrollbar
+export var measureScrollbar = function() { // https://github.com/twbs/bootstrap/blob/master/js/modal.js#L279-L286
+  var scrollDiv = document.createElement('div');
+  scrollDiv.style.width = '50px';
+  scrollDiv.style.height = '50px';
+  scrollDiv.style.overflow = 'scroll';
+  document.body.appendChild(scrollDiv);
+  var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+  document.body.removeChild(scrollDiv);
+  return scrollbarWidth;
 };

1c49d61d7f51fd2cd228845b4c7de0397bae4033
Limon Monte <limon.monte@gmail.com>
2016-09-18 20:53:43 +0200

    Add modal DOM observer to automatically change modal height

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -256,3 +256,18 @@ export var measureScrollbar = function() {
   document.body.removeChild(scrollDiv);
   return scrollbarWidth;
 };
+
+export var debounce = function(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};

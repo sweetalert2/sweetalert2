c14dece481494db037edeb7b02519fa2f2fadb52
Limon Monte <limon.monte@gmail.com>
2016-10-12 14:57:52 +0200

    Switch from ESLint to Standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,90 @@
-var gulp       = require('gulp');
-var cleanCSS   = require('gulp-clean-css');
-var sass       = require('gulp-sass');
-var rename     = require('gulp-rename');
-var autoprefix = require('gulp-autoprefixer');
-var eslint     = require('gulp-eslint');
-var sassLint   = require('gulp-sass-lint');
-var qunit      = require('gulp-qunit');
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css')
+var sass = require('gulp-sass')
+var rename = require('gulp-rename')
+var autoprefix = require('gulp-autoprefixer')
+var standard = require('gulp-standard')
+var sassLint = require('gulp-sass-lint')
+var qunit = require('gulp-qunit')
 
-var pack  = require('./package.json');
-var utils = require('./config/utils.js');
+var pack = require('./package.json')
+var utils = require('./config/utils.js')
 
-gulp.task('compress', ['eslint', 'commonjs', 'dev', 'production']);
+gulp.task('compress', ['standard', 'commonjs', 'dev', 'production'])
 
-gulp.task('eslint', function() {
+gulp.task('standard', function () {
   return gulp.src(['src/**/*.js', 'test/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true
+    }))
+})
 
-gulp.task('commonjs', function() {
+gulp.task('commonjs', function () {
   return utils.packageRollup({
     dest: 'dist/' + pack.name + '.common.js',
     format: 'cjs'
-  });
-});
+  })
+})
 
-gulp.task('dev', function() {
+gulp.task('dev', function () {
   return utils.packageRollup({
     dest: 'dist/' + pack.name + '.js',
     format: 'umd'
-  });
-});
+  })
+})
 
-gulp.task('test', function() {
+gulp.task('test', function () {
   return gulp.src('./test/test-runner.html')
     .pipe(qunit())
-    .on('error', function(err) { // avoid the ugly error message on failing
+    .on('error', function (err) { // avoid the ugly error message on failing
       if (process.env.CI) { // but still fail if we're running in a CI
-        throw err;
+        throw err
       }
-      this.emit('end');
-    });
-});
+      this.emit('end')
+    })
+})
 
-gulp.task('production', function() {
+gulp.task('production', function () {
   return utils.packageRollup({
     dest: 'dist/' + pack.name + '.min.js',
     format: 'umd',
     minify: true
-  }).then(utils.zip);
-});
+  }).then(utils.zip)
+})
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   gulp.src('src/sweetalert2.scss')
     .pipe(sass())
     .pipe(autoprefix())
     .pipe(gulp.dest('dist'))
     .pipe(cleanCSS())
     .pipe(rename({extname: '.min.css'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
 
   gulp.src('docs/example.scss')
     .pipe(sass())
     .pipe(autoprefix())
-    .pipe(gulp.dest('docs'));
-});
+    .pipe(gulp.dest('docs'))
+})
 
-gulp.task('sass-lint', function() {
+gulp.task('sass-lint', function () {
   return gulp.src(['src/**/*.scss', 'docs/**/*.scss'])
     .pipe(sassLint())
     .pipe(sassLint.format())
-    .pipe(sassLint.failOnError());
-});
+    .pipe(sassLint.failOnError())
+})
 
-gulp.task('default', ['compress', 'sass']);
+gulp.task('default', ['compress', 'sass'])
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch([
     'src/**/*.js',
     'test/*.js'
-  ], ['compress', 'test']);
+  ], ['compress', 'test'])
 
   gulp.watch([
     'src/sweetalert2.scss',
     'docs/example.scss'
-  ], ['sass-lint', 'sass']);
-});
+  ], ['sass-lint', 'sass'])
+})

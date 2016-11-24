6d0d2fa1652601fb17264c98421894971108aa90
Johan Fagerberg <johanringmann@gmail.com>
2016-11-24 20:23:57 +0100

    Gulp fixes (#358)
    * Fix #355
    
    * Fix #357
    
    * Fixed error callback not being bound to pipe
    
    * Test watcher now logs non-trivial errors
    
    * Undid removal of object-assign babel transform

--- gulpfile.js
+++ gulpfile.js
@@ -37,9 +37,14 @@ gulp.task('dev', () => {
 gulp.task('test', () => {
   return gulp.src('./test/test-runner.html')
     .pipe(qunit())
-    .on('error', (err) => { // avoid the ugly error message on failing
+    .on('error', function (err) { // avoid the ugly error message on failing
       if (process.env.CI) { // but still fail if we're running in a CI
         throw err
+      } else if (err.name !== "Error" || err.code !== 1) {
+        // rather crude filter
+        // this blocks the 'Command failed' error every time gulp-qunit fails
+        // logs all other errors without breaking the watcher
+        console.error(err.toString())
       }
       this.emit('end')
     })

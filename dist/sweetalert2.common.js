adbe4d7c30f7e971173cdce2a0ffca86a9f81dad
Limon Monte <limonte@users.noreply.github.com>
2016-12-28 13:02:50 +0200

    v6.2.7

--- dist/sweetalert2.common.js
+++ dist/sweetalert2.common.js
@@ -1,5 +1,5 @@
 /*!
- * sweetalert2 v6.2.6
+ * sweetalert2 v6.2.7
  * Released under the MIT License.
  */
 'use strict';
@@ -1701,7 +1701,7 @@ sweetAlert.resetDefaults = function () {
 
 sweetAlert.noop = function () {};
 
-sweetAlert.version = '6.2.6';
+sweetAlert.version = '6.2.7';
 
 module.exports = sweetAlert;
 if (window.Sweetalert2) window.sweetAlert = window.swal = window.Sweetalert2;

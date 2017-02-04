1d8b3997dfcba7c0c9ef06a7e27b6cdac2cf0d88
Limon Monte <limonte@users.noreply.github.com>
2017-02-04 16:07:00 +0200

    Fix scrollbar measurement for touch devices

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -250,6 +250,10 @@ export const resetPrevState = () => {
 // Measure width of scrollbar
 // https://github.com/twbs/bootstrap/blob/master/js/modal.js#L279-L286
 export const measureScrollbar = () => {
+  var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints
+  if (supportsTouch) {
+    return 0
+  }
   const scrollDiv = document.createElement('div')
   scrollDiv.style.width = '50px'
   scrollDiv.style.height = '50px'

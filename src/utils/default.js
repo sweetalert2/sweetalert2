9b8596f011c022b0c22bc8758fbf75e7c3603adb
Casey Webb <notcaseywebb@gmail.com>
2017-02-09 01:25:27 -0600

    Resolve issue when required from multiple bundles (#439)

--- src/utils/default.js
+++ src/utils/default.js
@@ -1,6 +1,4 @@
-import { swalClasses, iconTypes } from './classes.js'
-
-export const defaultParams = {
+export default {
   title: '',
   titleText: '',
   text: '',
@@ -45,52 +43,4 @@ export const defaultParams = {
   progressStepsDistance: '40px',
   onOpen: null,
   onClose: null
-}
-
-const sweetHTML = `
-  <div  role="dialog" aria-labelledby="modalTitleId" aria-describedby="modalContentId" class="${swalClasses.modal}" tabIndex="-1" >
-    <ul class="${swalClasses.progresssteps}"></ul>
-    <div class="${swalClasses.icon} ${iconTypes.error}">
-      <span class="x-mark"><span class="line left"></span><span class="line right"></span></span>
-    </div>
-    <div class="${swalClasses.icon} ${iconTypes.question}">?</div>
-    <div class="${swalClasses.icon} ${iconTypes.warning}">!</div>
-    <div class="${swalClasses.icon} ${iconTypes.info}">i</div>
-    <div class="${swalClasses.icon} ${iconTypes.success}">
-      <span class="line tip"></span> <span class="line long"></span>
-      <div class="placeholder"></div> <div class="fix"></div>
-    </div>
-    <img class="${swalClasses.image}">
-    <h2 class="${swalClasses.title}" id="modalTitleId"></h2>
-    <div id="modalContentId" class="${swalClasses.content}"></div>
-    <input class="${swalClasses.input}">
-    <input type="file" class="${swalClasses.file}">
-    <div class="${swalClasses.range}">
-      <output></output>
-      <input type="range">
-    </div>
-    <select class="${swalClasses.select}"></select>
-    <div class="${swalClasses.radio}"></div>
-    <label for="${swalClasses.checkbox}" class="${swalClasses.checkbox}">
-      <input type="checkbox">
-    </label>
-    <textarea class="${swalClasses.textarea}"></textarea>
-    <div class="${swalClasses.validationerror}"></div>
-    <hr class="${swalClasses.spacer}">
-    <button type="button" role="button" tabIndex="0" class="${swalClasses.confirm}">OK</button>
-    <button type="button" role="button" tabIndex="0" class="${swalClasses.cancel}">Cancel</button>
-    <span class="${swalClasses.close}">&times;</span>
-  </div>
-`.replace(/(^|\n)\s*/g, '')
-
-export let sweetContainer
-
-const existingSweetContainers = document.getElementsByClassName(swalClasses.container)
-
-if (existingSweetContainers.length) {
-  sweetContainer = existingSweetContainers[0]
-} else {
-  sweetContainer = document.createElement('div')
-  sweetContainer.className = swalClasses.container
-  sweetContainer.innerHTML = sweetHTML
 }
--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,8 +1,7 @@
 /* global MouseEvent */
 
 import { default as sweetAlert } from '../sweetalert2.js'
-import { swalPrefix, swalClasses } from './classes.js'
-import { sweetContainer } from './default.js'
+import { swalPrefix, swalClasses, iconTypes } from './classes.js'
 
 // Remember state in cases where opening and handling a modal will fiddle with it.
 export const states = {
@@ -22,7 +21,7 @@ export const init = () => {
     return
   }
 
-  document.body.appendChild(sweetContainer)
+  document.body.appendChild(getContainer())
 
   const modal = getModal()
   const input = getChildByClass(modal, swalClasses.input)
@@ -78,7 +77,55 @@ export const init = () => {
 /*
  * Manipulate DOM
  */
-export const elementByClass = (className) => sweetContainer.querySelector('.' + className)
+
+const sweetHTML = `
+ <div  role="dialog" aria-labelledby="modalTitleId" aria-describedby="modalContentId" class="${swalClasses.modal}" tabIndex="-1" >
+   <ul class="${swalClasses.progresssteps}"></ul>
+   <div class="${swalClasses.icon} ${iconTypes.error}">
+     <span class="x-mark"><span class="line left"></span><span class="line right"></span></span>
+   </div>
+   <div class="${swalClasses.icon} ${iconTypes.question}">?</div>
+   <div class="${swalClasses.icon} ${iconTypes.warning}">!</div>
+   <div class="${swalClasses.icon} ${iconTypes.info}">i</div>
+   <div class="${swalClasses.icon} ${iconTypes.success}">
+     <span class="line tip"></span> <span class="line long"></span>
+     <div class="placeholder"></div> <div class="fix"></div>
+   </div>
+   <img class="${swalClasses.image}">
+   <h2 class="${swalClasses.title}" id="modalTitleId"></h2>
+   <div id="modalContentId" class="${swalClasses.content}"></div>
+   <input class="${swalClasses.input}">
+   <input type="file" class="${swalClasses.file}">
+   <div class="${swalClasses.range}">
+     <output></output>
+     <input type="range">
+   </div>
+   <select class="${swalClasses.select}"></select>
+   <div class="${swalClasses.radio}"></div>
+   <label for="${swalClasses.checkbox}" class="${swalClasses.checkbox}">
+     <input type="checkbox">
+   </label>
+   <textarea class="${swalClasses.textarea}"></textarea>
+   <div class="${swalClasses.validationerror}"></div>
+   <hr class="${swalClasses.spacer}">
+   <button type="button" role="button" tabIndex="0" class="${swalClasses.confirm}">OK</button>
+   <button type="button" role="button" tabIndex="0" class="${swalClasses.cancel}">Cancel</button>
+   <span class="${swalClasses.close}">&times;</span>
+ </div>
+`.replace(/(^|\n)\s*/g, '')
+
+export const getContainer = () => {
+  const existingContainers = document.getElementsByClassName(swalClasses.container)
+
+  if (existingContainers.length) {
+    return existingContainers[0]
+  } else {
+    const container = document.createElement('div')
+    container.className = swalClasses.container
+    container.innerHTML = sweetHTML
+    return container
+  }
+}
 
 export const getModal = () => document.body.querySelector('.' + swalClasses.modal) || init()
 
@@ -86,6 +133,8 @@ export const getIcons = () => {
   const modal = getModal()
   return modal.querySelectorAll('.' + swalClasses.icon)
 }
+
+export const elementByClass = (className) => getContainer().querySelector('.' + className)
 
 export const getTitle = () => elementByClass(swalClasses.title)
 

884e3524caf33b8208ea7bd386e007ccc882910d
Limon Monte <limon.monte@gmail.com>
2017-12-18 17:36:54 +0200

    7.1.1

--- dist/sweetalert2.js
+++ dist/sweetalert2.js
@@ -1,5 +1,5 @@
 /*!
- * sweetalert2 v7.1.0
+ * sweetalert2 v7.1.1
  * Released under the MIT License.
  */
 (function (global, factory) {
@@ -1180,7 +1180,37 @@ var sweetAlert = function sweetAlert() {
         }
       };
     } else {
+      var ignoreOutsideClick = false;
+
+      // Ignore click events that had mousedown on the popup but mouseup on the container
+      // This can happen when the user drags a slider
+      popup.onmousedown = function () {
+        container.onmouseup = function (e) {
+          container.onmouseup = undefined;
+          // We only check if the mouseup target is the container because usually it doesn't
+          // have any other direct children aside of the popup
+          if (e.target === container) {
+            ignoreOutsideClick = true;
+          }
+        };
+      };
+
+      // Ignore click events that had mousedown on the container but mouseup on the popup
+      container.onmousedown = function () {
+        popup.onmouseup = function (e) {
+          popup.onmouseup = undefined;
+          // We also need to check if the mouseup target is a child of the popup
+          if (e.target === popup || popup.contains(e.target)) {
+            ignoreOutsideClick = true;
+          }
+        };
+      };
+
       container.onclick = function (e) {
+        if (ignoreOutsideClick) {
+          ignoreOutsideClick = false;
+          return;
+        }
         if (e.target !== container) {
           return;
         }
@@ -1830,7 +1860,7 @@ sweetAlert.adaptInputValidator = function (legacyValidator) {
 
 sweetAlert.noop = function () {};
 
-sweetAlert.version = '7.1.0';
+sweetAlert.version = '7.1.1';
 
 sweetAlert.default = sweetAlert;
 

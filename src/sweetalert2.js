sweetalert2/sweetalert2

commit 418405a398d9ceaddadeef984f017af72587c673

Added window._swalDefaults support, closes #763 (#769)

src/sweetalert2.js
@@ -1327,4 +1327,11 @@ sweetAlert.version = ''
 
 sweetAlert.default = sweetAlert
 
+/**
+ * Set default params if `window._swalDefaults` is an object
+ */
+if (typeof window._swalDefaults === 'object') {
+  sweetAlert.setDefaults(window._swalDefaults);
+}
+
 export default sweetAlert

commit ce6529dba100c38a2d8bfc0fbdd1f0bc33cb3be2

Fix SSR after it was broken in #769

src/sweetalert2.js
@@ -1358,7 +1358,7 @@ sweetAlert.default = sweetAlert
 /**
  * Set default params if `window._swalDefaults` is an object
  */
-if (typeof window._swalDefaults === 'object') {
+if (typeof window !== 'undefined' && typeof window._swalDefaults === 'object') {
   sweetAlert.setDefaults(window._swalDefaults)
 }
 
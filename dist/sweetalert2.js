2ae9afbb525cf9f10877e7fa616284e8e2841639
Limon Monte <limonte@users.noreply.github.com>
2018-01-20 09:58:47 +0200

    7.5.1

--- dist/sweetalert2.js
+++ dist/sweetalert2.js
@@ -1,5 +1,5 @@
 /*!
- * sweetalert2 v7.5.0
+ * sweetalert2 v7.5.1
  * Released under the MIT License.
  */
 (function (global, factory) {
@@ -1891,7 +1891,7 @@ sweetAlert.adaptInputValidator = function (legacyValidator) {
 
 sweetAlert.noop = function () {};
 
-sweetAlert.version = '7.5.0';
+sweetAlert.version = '7.5.1';
 
 sweetAlert.default = sweetAlert;
 

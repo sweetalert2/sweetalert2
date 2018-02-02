7189f825290a7abb24877200ba0476edc5924fd4
Limon Monte <limon.monte@gmail.com>
2018-02-02 23:18:09 +0200

    feat(release): use npm version instead of manual commits (#880)

--- utils/get-git-tags.js
+++
@@ -1,8 +0,0 @@
-const execute = require('./execute')
-
-async function getGitTags () {
-  const {stdout} = await execute('git tag', {skipLogging: true})
-  return stdout.split(/\r?\n/).filter(Boolean)
-}
-
-module.exports = getGitTags

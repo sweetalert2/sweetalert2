138b5c87e84cb73e917c2efb81a5578b17067391
Limon Monte <limon.monte@gmail.com>
2018-02-26 10:26:47 +0100

    Fix IE11 unit tests and enable them in AppVeyor CI (#953)
    * chore(appveyor): npm -> yarn
    
    * fix(ie11-tests): add ugly timeout for IE11 tests
    
    * chore(appveyor): re-enable IE tests
    
    * chore(tests): do not use tests timeouts for non-ie browsers

--- test/qunit/deprecated.js
+++ test/qunit/deprecated.js
@@ -1,5 +1,6 @@
 /* global QUnit, swal */
 const $ = require('jquery')
+import { TIMEOUT } from './helpers.js'
 
 QUnit.test('confirm button /w useRejections: true', (assert) => {
   const done = assert.async()
@@ -196,6 +197,6 @@ QUnit.test('validation error /w expectRejections: true', (assert) => {
       assert.notOk($('.swal2-input').attr('aria-invalid'))
       assert.ok($('.swal2-modal').outerHeight() === initialModalHeight)
       done()
-    })
-  }, 60)
+    }, TIMEOUT)
+  }, TIMEOUT)
 })

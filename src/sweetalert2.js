b881cebed35bec009289fd57e43407ee66d89270
Limon Monte <limon.monte@gmail.com>
2018-03-06 02:23:38 +0200

    chore: improve readability, remove double brackets

--- src/sweetalert2.js
+++ src/sweetalert2.js
@@ -330,7 +330,12 @@ const sweetAlert = (...args) => {
     if (params.toast) {
       // Closing popup by internal click
       domCache.popup.onclick = (e) => {
-        if ((params.showConfirmButton || params.showCancelButton || params.showCloseButton || params.input)) {
+        if (
+          params.showConfirmButton ||
+          params.showCancelButton ||
+          params.showCloseButton ||
+          params.input
+        ) {
           return
         }
         sweetAlert.closePopup(params.onClose)

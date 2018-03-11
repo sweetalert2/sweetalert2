92f79f661cddb2b8e25f1689b19cd129e457ca75
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-10 22:41:48 -0300

    feat(api): add mixin method

--- src/sweetalert2.js
+++ src/sweetalert2.js
@@ -1037,6 +1037,31 @@ sweetAlert.argsToParams = (args) => {
   return params
 }
 
+/**
+ * Returns a wrapped instance of `swal` containing `params` as defaults.
+ * Useful for reusing swal configuration.
+ *
+ * For example:
+ *
+ * Before:
+ * const textPromptOptions = { input: 'text', showCancelButton: true }
+ * const {value: firstName} = await swal({ ...textPromptOptions, title: 'What is your first name?' })
+ * const {value: lastName} = await swal({ ...textPromptOptions, title: 'What is your last name?' })
+ *
+ * After:
+ * const myTextPrompt = swal.mixin({ input: 'text', showCancelButton: true })
+ * const {value: firstName} = await myTextPrompt('What is your first name?')
+ * const {value: lastName} = await myTextPrompt('What is your last name?')
+ *
+ * @param params
+ */
+sweetAlert.mixin = function (params) {
+  const parentSwal = this
+  const childSwal = (...args) =>
+    parentSwal(Object.assign({}, params, parentSwal.argsToParams(args)))
+  return Object.assign(childSwal, parentSwal)
+}
+
 sweetAlert.DismissReason = DismissReason
 
 sweetAlert.noop = () => { }

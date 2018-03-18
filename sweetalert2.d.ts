37fce3f1afa78d6c49b18f3abddd0b4f348579aa
Rafael Santana <rafaelsantana3095@gmail.com>
2018-03-18 04:45:13 -0300

    chore(d.ts): improve type safety for 'inputValue' attribute (#981)

--- sweetalert2.d.ts
+++ sweetalert2.d.ts
@@ -628,7 +628,7 @@ declare module 'sweetalert2' {
          *
          * @default ''
          */
-        inputValue?: any;
+        inputValue?: string;
 
         /**
          * If input parameter is set to "select" or "radio", you can provide options.
@@ -672,7 +672,7 @@ declare module 'sweetalert2' {
          *
          * @default null
          */
-        inputValidator?: (inputValue: any) => SyncOrAsync<string | null>;
+        inputValidator?: (inputValue: string) => SyncOrAsync<string | null>;
 
         /**
          * A custom CSS class for the input field.

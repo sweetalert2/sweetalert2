004f1c4e98e567bf30e4d44a3a5a2ad8455c81f8
Limon Monte <limon.monte@gmail.com>
2018-03-29 15:29:11 +0300

    chore: mention author of todos [skip ci]

--- src/instanceMethods/_main.js
+++ src/instanceMethods/_main.js
@@ -33,7 +33,7 @@ export function _main (userParams) {
   return new Promise((resolve, reject) => {
     // functions to handle all resolving/rejecting/settling
     const succeedWith = (value) => {
-      constructor.closePopup(innerParams.onClose, innerParams.onAfterClose) // TODO: make closePopup an *instance* method
+      constructor.closePopup(innerParams.onClose, innerParams.onAfterClose) // @todo (zenflow): make closePopup an *instance* method
       if (innerParams.useRejections) {
         resolve(value)
       } else {
@@ -88,7 +88,7 @@ export function _main (userParams) {
 
     const confirm = (value) => {
       if (innerParams.showLoaderOnConfirm) {
-        constructor.showLoading() // TODO: make showLoading an *instance* method
+        constructor.showLoading() // @todo (zenflow): make showLoading an *instance* method
       }
 
       if (innerParams.preConfirm) {

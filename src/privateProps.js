0ec90341b01c73c95e8a4709bd9a5a7b60108719
Limon Monte <limon.monte@gmail.com>
2018-05-03 07:08:34 +0900

    Add Android 4.4 support (#1072)
    * feat(tests): add Android KitKat launcher to SauceLabs tests
    
    * fix: add WeakMap and Symbol polyfills for Android 4.4
    
    * fix: use __proto__ instead of setPrototypeOf() for Android 4.4
    
    * fix: check Map existence before using it (in Android 4.4 it's not available)
    
    * fix(tests): use TIMEOUT 500ms for Android devices (fix #1073)
    
    * fix(test): increase karma captureTimeout because of SauceLabs iOS

--- src/privateProps.js
+++ src/privateProps.js
@@ -7,6 +7,40 @@
  * Once we have the changes from that PR in Babel, and our core class fits reasonable in *one module*
  *   then we can use that language feature.
  */
+
+// WeakMap polyfill, needed for Android 4.4
+// Related issue: https://github.com/sweetalert2/sweetalert2/issues/1071
+if (typeof window !== 'undefined' && typeof window.WeakMap !== 'function') {
+  // https://github.com/Riim/symbol-polyfill/blob/master/index.js
+  let idCounter = 0
+  window.Symbol = function Symbol (key) {
+    return '__' + key + '_' + Math.floor(Math.random() * 1e9) + '_' + (++idCounter) + '__'
+  }
+  Symbol.iterator = Symbol('Symbol.iterator')
+
+  // http://webreflection.blogspot.fi/2015/04/a-weakmap-polyfill-in-20-lines-of-code.html
+  window.WeakMap = (function (s, dP, hOP) {
+    function WeakMap () {
+      dP(this, s, {value: Symbol('WeakMap')})
+    }
+    WeakMap.prototype = {
+      'delete': function del (o) {
+        delete o[this[s]]
+      },
+      get: function get (o) {
+        return o[this[s]]
+      },
+      has: function has (o) {
+        return hOP.call(o, this[s])
+      },
+      set: function set (o, v) {
+        dP(o, this[s], {configurable: true, value: v})
+      }
+    }
+    return WeakMap
+  }(Symbol('WeakMap'), Object.defineProperty, {}.hasOwnProperty))
+}
+
 export default {
   promise: new WeakMap(),
   innerParams: new WeakMap(),

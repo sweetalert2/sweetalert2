9c04ae614c92501285ad5e992f5531304fd89ab6
Limon Monte <limonte@users.noreply.github.com>
2018-05-12 01:14:07 +0300

    fix: default validation messages

--- src/utils/defaultInputValidators.js
+++ src/utils/defaultInputValidators.js
@@ -2,12 +2,12 @@ export default {
   email: (string, extraParams) => {
     return /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9-]{2,24}$/.test(string)
       ? Promise.resolve()
-      : Promise.reject(extraParams.validationMessage || 'Invalid email address')
+      : Promise.reject(extraParams && extraParams.validationMessage ? extraParams.validationMessage : 'Invalid email address')
   },
   url: (string, extraParams) => {
     // taken from https://stackoverflow.com/a/3809435/1331425
     return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/.test(string)
       ? Promise.resolve()
-      : Promise.reject(extraParams.validationMessage || 'Invalid URL')
+      : Promise.reject(extraParams && extraParams.validationMessage ? extraParams.validationMessage : 'Invalid URL')
   }
 }

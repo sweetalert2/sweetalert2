sweetalert2/sweetalert2

commit d6fa35417bd4c5a467386e312bd9c305c486158c

fix: check if keydown event happend inside the input (#1097)

src/instanceMethods/_main.js
@@ -299,7 +299,7 @@ export function _main (userParams) {
       ]
 
       if (e.key === 'Enter' && !e.isComposing) {
-        if (e.target === this.getInput()) {
+        if (e.target.outerHTML === this.getInput().outerHTML) {
           if (['textarea', 'file'].includes(innerParams.input)) {
             return // do not submit
           }

commit 0747a0fce4747ce4eec58a2e001abffd7cb1e804

fix ngx-sweetalert2#75, regression after d6fa3541

src/instanceMethods/_main.js
@@ -299,7 +299,7 @@ export function _main (userParams) {
       ]
 
       if (e.key === 'Enter' && !e.isComposing) {
-        if (e.target.outerHTML === this.getInput().outerHTML) {
+        if (e.target && this.getInput() && e.target.outerHTML === this.getInput().outerHTML) {
           if (['textarea', 'file'].includes(innerParams.input)) {
             return // do not submit
           }
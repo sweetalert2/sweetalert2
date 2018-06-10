47b50213a0c353c725b160aeb23b46513aea6e66
Limon Monte <limon.monte@gmail.com>
2018-06-10 11:57:57 +0300

    fix: unset previousActiveElement in restoreFocusTimeout, not before (#1125)

--- src/globalState.js
+++ src/globalState.js
@@ -5,12 +5,11 @@ export default globalState
 // Restore previous active (focused) element
 export const restoreActiveElement = () => {
   if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
-    const previousActiveElement = globalState.previousActiveElement
-    globalState.previousActiveElement = null
     const x = window.scrollX
     const y = window.scrollY
     globalState.restoreFocusTimeout = setTimeout(() => {
-      previousActiveElement.focus && previousActiveElement.focus()
+      globalState.previousActiveElement.focus()
+      globalState.previousActiveElement = null
     }, 100) // issues/900
     if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support
       window.scrollTo(x, y)

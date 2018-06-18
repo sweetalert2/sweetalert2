67f5929f514df23edf98e2824341a6cf35851e2e
Limon Monte <limon.monte@gmail.com>
2018-06-18 20:05:28 +0300

    chore: set Android tests timeout to 500, revert change from 5ec98d0

--- test/qunit/helpers.js
+++ test/qunit/helpers.js
@@ -4,8 +4,10 @@ const browser = detect()
 
 export let TIMEOUT = 1
 
-if (browser.name === 'ie' || browser.os === 'Android OS') {
+if (browser.name === 'ie') {
   TIMEOUT = 100
+} else if (browser.os === 'Android OS') {
+  TIMEOUT = 500
 }
 
 // We *only* access `Swal` through this module, so that we can be sure `initialSwalPropNames` is set properly

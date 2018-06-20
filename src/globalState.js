61c52264fd0f50796a3660cfcf08ec6f26262f2e
Limon Monte <limonte@users.noreply.github.com>
2018-06-20 21:54:36 +0300

    fix: check if previousActiveElement is valid before focusing it (#1128)
    Check if previousActiveElement is valid before focusing it

--- src/globalState.js
+++ src/globalState.js
@@ -4,15 +4,15 @@ export default globalState
 
 // Restore previous active (focused) element
 export const restoreActiveElement = () => {
-  if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
-    const x = window.scrollX
-    const y = window.scrollY
-    globalState.restoreFocusTimeout = setTimeout(() => {
+  const x = window.scrollX
+  const y = window.scrollY
+  globalState.restoreFocusTimeout = setTimeout(() => {
+    if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
       globalState.previousActiveElement.focus()
       globalState.previousActiveElement = null
-    }, 100) // issues/900
-    if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support
-      window.scrollTo(x, y)
     }
+  }, 100) // issues/900
+  if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support
+    window.scrollTo(x, y)
   }
 }

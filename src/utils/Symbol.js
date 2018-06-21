cff3de5278378143d7fa04c0e5a7a01d0f240a09
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-06-21 12:46:18 -0300

    fix(polyfill): Do not monkey patch Symbol and WeakMap polyfills onto global object (#1133)

---
+++ src/utils/Symbol.js
@@ -0,0 +1,10 @@
+// https://github.com/Riim/symbol-polyfill/blob/master/index.js
+
+export default typeof Symbol === 'function' ? Symbol : (() => {
+  let idCounter = 0
+  function Symbol (key) {
+    return '__' + key + '_' + Math.floor(Math.random() * 1e9) + '_' + (++idCounter) + '__'
+  }
+  Symbol.iterator = Symbol('Symbol.iterator')
+  return Symbol
+})()

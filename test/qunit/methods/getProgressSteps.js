97cd2282d4a86113d9afe9c6f128537a49f72ebb
Limon Monte <limon.monte@gmail.com>
2018-07-13 14:29:02 +0300

    fix(api): include progress-steps methods

---
+++ test/qunit/methods/getProgressSteps.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+import { Swal } from '../helpers.js'
+
+QUnit.test('getProgressSteps() method', (assert) => {
+  Swal({ progressSteps: ['1', '2', '3'] })
+  const progressSteps = Swal.getProgressSteps()
+  assert.deepEqual(progressSteps, ['1', '2', '3'])
+})

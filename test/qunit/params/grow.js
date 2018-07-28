abe9751f8301011880cc74b90d24f6ce3e25c02f
Limon Monte <limonte@users.noreply.github.com>
2018-07-29 01:47:26 +0300

    fix(api): fix grow fullscreen

---
+++ test/qunit/params/grow.js
@@ -0,0 +1,40 @@
+const {Swal} = require('../helpers')
+
+QUnit.test('grow row', (assert) => {
+  Swal({
+    grow: 'row'
+  })
+  const containerStyles = window.getComputedStyle(Swal.getContainer())
+  assert.equal(
+    Swal.getPopup().clientWidth,
+    Swal.getContainer().clientWidth - parseInt(containerStyles.paddingLeft) - parseInt(containerStyles.paddingRight)
+  )
+})
+
+QUnit.test('grow column', (assert) => {
+  Swal({
+    grow: 'column'
+  })
+  const containerStyles = window.getComputedStyle(Swal.getContainer())
+  assert.equal(
+    Swal.getPopup().clientHeight,
+    Swal.getContainer().clientHeight - parseInt(containerStyles.paddingTop) - parseInt(containerStyles.paddingBottom)
+  )
+})
+
+QUnit.test('grow fullscreen', (assert) => {
+  Swal({
+    grow: 'fullscreen'
+  })
+  const containerStyles = window.getComputedStyle(Swal.getContainer())
+
+  assert.equal(
+    Swal.getPopup().clientWidth,
+    Swal.getContainer().clientWidth - parseInt(containerStyles.paddingLeft) - parseInt(containerStyles.paddingRight)
+  )
+
+  assert.equal(
+    Swal.getPopup().clientHeight,
+    Swal.getContainer().clientHeight - parseInt(containerStyles.paddingTop) - parseInt(containerStyles.paddingBottom)
+  )
+})

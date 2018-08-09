a5a9bd3cf6447bbd2bbb50e8051bd9356a6b6cd4
Limon Monte <limonte@users.noreply.github.com>
2018-08-09 13:56:40 +0300

    feat(d.ts): add getTimerLeft()

--- src/staticMethods/timer.js
+++ src/staticMethods/timer.js
@@ -1,7 +1,8 @@
 import globalState from '../globalState'
 
 /**
- * Show spinner instead of Confirm button and disable Cancel button
+ * If `timer` parameter is set, returns number os milliseconds of timer remained.
+ * Otherwise, returns null.
  */
 export const getTimerLeft = () => {
   return globalState.timeout && globalState.timeout.getTimerLeft()

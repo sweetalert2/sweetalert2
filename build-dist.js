36bb018ff5b19e999ad5c56fe4a2136e43f5d57e
Limon Monte <limonte@users.noreply.github.com>
2018-09-06 15:18:09 +0300

    chore(semantic-release): build dist in semantic-release

--- build-dist.js
+++ build-dist.js
@@ -6,22 +6,9 @@ const execute = require('./utils/execute')
 const log = console.log // eslint-disable-line
 const removeDir = pify(rimraf)
 
+const version = process.argv[2]
+
 ;(async () => {
-  log('Resetting the current branch...')
-  await execute('git fetch')
-  await execute('git checkout .')
-
-  if (isCi) {
-    // https://stackoverflow.com/a/47441734
-    log('Allow access to other branches...')
-    await execute('git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*')
-    await execute('git fetch --unshallow --tags')
-  }
-
-  log('Switching to the dist branch...')
-  await execute('git checkout -B dist origin/dist')
-  await execute('git merge --strategy-option=theirs master --no-ff')
-
   log('Deleting the current dist folder...')
   await removeDir('dist')
 
@@ -29,15 +16,15 @@ const removeDir = pify(rimraf)
   await execute('yarn build')
 
   log('Committing the dist dir...')
-  await execute(`git add --force dist/ && git commit -m "chore: add dist"`)
+  await execute(`git add --force dist/ && git commit -m "chore: add dist for ${version}"`)
 
-  log('Pushing to Github both master and dist branches...')
+  log('Pushing to Github ...')
   if (isCi) {
     await execute('git config --global user.email "semantic-release-bot@martynus.net"')
     await execute('git config --global user.name "semantic-release-bot"')
     await execute(`git remote set-url origin https://${process.env.GH_TOKEN}@github.com/sweetalert2/sweetalert2.git`)
   }
-  await execute('git push origin dist')
+  await execute('git push origin')
 
   log('OK!')
 })().catch(console.error)

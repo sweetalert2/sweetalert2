176b42331bd903a1b902f96b1110f2641ad89591
Limon Monte <limon.monte@gmail.com>
2018-09-28 09:30:06 +0300

    fix(api): call onAfterClose after previousActiveElement is focused

--- src/globalState.js
+++ src/globalState.js
@@ -6,17 +6,20 @@ export default globalState
 
 // Restore previous active (focused) element
 export const restoreActiveElement = () => {
-  const x = window.scrollX
-  const y = window.scrollY
-  globalState.restoreFocusTimeout = setTimeout(() => {
-    if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
-      globalState.previousActiveElement.focus()
-      globalState.previousActiveElement = null
-    } else if (document.body) {
-      document.body.focus()
+  return new Promise(resolve => {
+    const x = window.scrollX
+    const y = window.scrollY
+    globalState.restoreFocusTimeout = setTimeout(() => {
+      if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
+        globalState.previousActiveElement.focus()
+        globalState.previousActiveElement = null
+      } else if (document.body) {
+        document.body.focus()
+      }
+      resolve()
+    }, RESTORE_FOCUS_TIMEOUT) // issues/900
+    if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support
+      window.scrollTo(x, y)
     }
-  }, RESTORE_FOCUS_TIMEOUT) // issues/900
-  if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support
-    window.scrollTo(x, y)
-  }
+  })
 }

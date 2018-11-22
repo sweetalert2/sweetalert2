224bf72be50a6e15ce7643d34b50ade8587eecc2
gverni <g.verni@gmail.com>
2018-11-22 18:33:45 +0000

    Removed first leveel depency from event-stream

--- utils/execute.js
+++ utils/execute.js
@@ -1,17 +1,60 @@
 const execa = require('execa')
-const es = require('event-stream')
+
+
+const Stream = require('stream').Stream
+const split = require('split') // dominictarr
+const map = require('map-stream') //dominictarr 
+
+function merge (/*streams...*/) {
+  var toMerge = [].slice.call(arguments)
+  if (toMerge.length === 1 && (toMerge[0] instanceof Array)) {
+    toMerge = toMerge[0] //handle array as arguments object
+  }
+  var stream = new Stream()
+  stream.setMaxListeners(0) // allow adding more than 11 streams
+  var endCount = 0
+  stream.writable = stream.readable = true
+
+  if (toMerge.length) {
+    toMerge.forEach(function (e) {
+      e.pipe(stream, {end: false})
+      var ended = false
+      e.on('end', function () {
+        if(ended) return
+        ended = true
+        endCount ++
+        if(endCount == toMerge.length)
+          stream.emit('end')
+      })
+    })
+  } else {
+    process.nextTick(function () {
+      stream.emit('end')
+    })
+  }
+  
+  stream.write = function (data) {
+    this.emit('data', data)
+  }
+  stream.destroy = function () {
+    toMerge.forEach(function (e) {
+      if(e.destroy) e.destroy()
+    })
+  }
+  return stream
+}
 
 module.exports = function execute (command, { skipLogging } = {}) {
   const proc = execa.shell(command)
-
+  console.log('running execute....')
   if (!skipLogging) {
     const formatLine = (line, label) => `    (pid:${proc.pid})\t[${label}]\t${line}\n`
     const formatStream = (stream, label) => stream
-      .pipe(es.split())
-      .pipe(es.map((line, cb) => cb(null, formatLine(line, label))))
+      .pipe(split())
+      .pipe(map((line, cb) => cb(null, formatLine(line, label))))
 
     process.stdout.write(formatLine(command, 'cmd'))
-    es.merge(formatStream(proc.stdout, 'out'), formatStream(proc.stderr, 'ERR'))
+    merge(formatStream(proc.stdout, 'out'), formatStream(proc.stderr, 'ERR'))
       .pipe(process.stdout)
   }
 

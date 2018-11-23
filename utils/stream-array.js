5b059c94b86257b8cbdf07daa6502e5ba45db178
Giuseppe Verni <g.verni@gmail.com>
2018-11-23 21:42:11 +0000

    Fixed lint errors

--- utils/stream-array.js
+++ utils/stream-array.js
@@ -19,19 +19,19 @@ function split () {
 }
 
 function merge (/*streams...*/) {
-  var toMerge = [].slice.call(arguments)
+  let toMerge = [].slice.call(arguments)
   if (toMerge.length === 1 && (toMerge[0] instanceof Array)) {
     toMerge = toMerge[0] //handle array as arguments object
   }
-  var stream = new Stream()
+  let stream = new Stream()
   stream.setMaxListeners(0) // allow adding more than 11 streams
-  var endCount = 0
+  let endCount = 0
   stream.writable = stream.readable = true
 
   if (toMerge.length) {
     toMerge.forEach(function (e) {
-      e.pipe(stream, {end: false})
-      var ended = false
+      e.pipe(stream, { end: false })
+      let ended = false
       e.on('end', function () {
         if(ended) return
         ended = true

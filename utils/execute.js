3cc8a2a6e141b163d40228316e74469253dcb3dc
gverni <g.verni@gmail.com>
2018-11-26 12:59:41 +0000

    chore: remove event-stream package (#1305)
    * Removed first leveel depency from event-stream
    
    * Moved streams functions in stream-array
    
    * Removed log message
    
    * Removed dependency from split package
    
    For the pourpose of this project, the `split` package is used
    for buffering data in the stream until
    a `\n` or / and `\r` is received.
    Once this happens, the buffer is flushed in the stream.
    
    This is a very basic implementation of this mechanism.
    
    * Fixed lint errors
    
    * Handle more than one  in a single chunk
    
    * Removed dependency from map-stream
    
    Added a duplex stream that executes the function passed as argument
    on the chunk
    
    * Code clean-up
    
    * Reduced code for `merge`
    
    This implemnt a cut down version of the merge function, with the
    following assumptions:
    * The argument is alway 2 or more stream
    * The input stream never emits `end`
    * The merged stream never receive `destroy`
    
    * Fixed lint error
    
    * Removed dependency to event-stream
    
    * Updates to yarn.lock after event-stream removal

--- utils/execute.js
+++ utils/execute.js
@@ -1,17 +1,16 @@
 const execa = require('execa')
-const es = require('event-stream')
+const streamArray = require('./stream-array')
 
 module.exports = function execute (command, { skipLogging } = {}) {
   const proc = execa.shell(command)
-
   if (!skipLogging) {
     const formatLine = (line, label) => `    (pid:${proc.pid})\t[${label}]\t${line}\n`
     const formatStream = (stream, label) => stream
-      .pipe(es.split())
-      .pipe(es.map((line, cb) => cb(null, formatLine(line, label))))
+      .pipe(streamArray.split())
+      .pipe(streamArray.map(line => formatLine(line, label)))
 
     process.stdout.write(formatLine(command, 'cmd'))
-    es.merge(formatStream(proc.stdout, 'out'), formatStream(proc.stderr, 'ERR'))
+    streamArray.merge(formatStream(proc.stdout, 'out'), formatStream(proc.stderr, 'ERR'))
       .pipe(process.stdout)
   }
 

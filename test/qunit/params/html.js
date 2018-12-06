6f35e48b6ac8678cc92f6ad84707ab64747935da
Limon Monte <limon.monte@gmail.com>
2018-12-06 19:52:59 +0200

    fix: support HTMLElement for setting title/html/footer (#1328)

---
+++ test/qunit/params/html.js
@@ -0,0 +1,14 @@
+const { $, Swal } = require('../helpers')
+
+QUnit.test('HTMLElement as html', (assert) => {
+  const form = document.createElement('form')
+  const div = document.createElement('div')
+  div.appendChild(document.createElement('label'))
+  div.appendChild(document.createElement('input'))
+  form.appendChild(div)
+
+  Swal({
+    html: form
+  })
+  assert.equal($('#swal2-content').innerHTML, '<form><div><label></label><input></div></form>')
+})

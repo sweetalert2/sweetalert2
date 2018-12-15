ac7698acc35c253539070e29753b5805174b82b1
gverni <g.verni@gmail.com>
2018-12-15 00:00:37 +0000

    Added positioning test

--- test/qunit/params/target.js
+++ test/qunit/params/target.js
@@ -27,7 +27,29 @@ QUnit.test('target', (assert) => {
   Swal({ title: 'Custom invalid target (element)', target: true })
   assert.equal(document.body, document.querySelector('.swal2-container').parentNode)
   Swal.close()
+
+  dummyTargetElement.parentNode.removeChild(dummyTargetElement) // Remove target element before next test
   console.warn = warn // Suppress the warnings
 })
 
+QUnit.test('target positioning: bottom-end', (assert) => {
+  const warn = console.warn // Suppress the warnings
+  console.warn = () => true // Suppress the warnings
 
+  const targetWidth = 600
+  const targetHeight = 300
+
+  const dummyTargetElement = Object.assign(document.createElement('div'), { id: 'dummy-target' })
+  dummyTargetElement.setAttribute('style', `width: ${targetWidth}px; height: ${targetHeight}px`)
+  document.body.appendChild(dummyTargetElement)
+
+  Swal({ title: 'Custom target', target: '#dummy-target', position: 'bottom-end', animation: 'false' })
+  const swalBottomPos = document.querySelector('.swal2-popup').getBoundingClientRect().bottom
+  const swalRightPos = document.querySelector('.swal2-popup').getBoundingClientRect().right
+  assert.ok(swalBottomPos === targetHeight - 2)
+  assert.ok(swalRightPos === targetWidth - 2)
+  Swal.close()
+
+  dummyTargetElement.parentNode.removeChild(dummyTargetElement) // Remove target element before next test
+  console.warn = warn // Suppress the warnings
+})

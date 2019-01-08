a7f655b969c12f77a098551894870b29efe0ffcd
Limon Monte <limonte@users.noreply.github.com>
2019-01-08 18:44:25 +0200

    BREAKING CHANGE: remove showValidationError and resetValidationError (#1367)
    BREAKING CHANGE: remove showValidationError and resetValidationError (#1367)

--- src/instanceMethods/show-reset-validation-error.js
+++ src/instanceMethods/show-reset-validation-error.js
@@ -1,5 +1,4 @@
 import * as dom from '../utils/dom/index'
-import { warnOnce } from '../utils/utils'
 import { swalClasses } from '../utils/classes'
 import privateProps from '../privateProps'
 
@@ -34,18 +33,4 @@ export function resetValidationMessage () {
     input.removeAttribute('aria-describedBy')
     dom.removeClass(input, swalClasses.inputerror)
   }
-}
-
-// @deprecated
-/* istanbul ignore next */
-export function resetValidationError() {
-  warnOnce(`Swal.resetValidationError() is deprecated and will be removed in the next major release, use Swal.resetValidationMessage() instead`)
-  resetValidationMessage.bind(this)()
-}
-
-// @deprecated
-/* istanbul ignore next */
-export function showValidationError(error) {
-  warnOnce(`Swal.showValidationError() is deprecated and will be removed in the next major release, use Swal.showValidationMessage() instead`)
-  showValidationMessage.bind(this)(error)
 }

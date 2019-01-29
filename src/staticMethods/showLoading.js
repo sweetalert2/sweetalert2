d9b33b973578ba8766a301fa64041a0200585779
Limon Monte <limonte@users.noreply.github.com>
2019-01-29 09:11:00 +0200

    fix(api): showLoading() should open a new popup (#1394)

--- src/staticMethods/showLoading.js
+++ src/staticMethods/showLoading.js
@@ -1,5 +1,5 @@
 import * as dom from '../utils/dom/index.js'
-import sweetAlert from '../sweetalert2.js'
+import Swal from '../sweetalert2.js'
 import { swalClasses } from '../utils/classes.js'
 
 /**
@@ -8,7 +8,7 @@ import { swalClasses } from '../utils/classes.js'
 const showLoading = () => {
   let popup = dom.getPopup()
   if (!popup) {
-    sweetAlert('')
+    Swal.fire('')
   }
   popup = dom.getPopup()
   const actions = dom.getActions()

f1a22591797250bbe8bdfbb219d8cfe7b9417306
Benjamin Parish <bparish@d3banking.com>
2019-02-23 08:53:50 -0600

    fix: padding 0 (#1424)
    * Fix issue with padding not allowing 0.
    * Added string to typing (sweetalerts2 already checks for this when applying padding)
    * Adding missing padding tests.

---
+++ test/qunit/params/padding.js
@@ -0,0 +1,50 @@
+const { Swal } = require('../helpers')
+
+QUnit.test('padding should allow 0', (assert) => {
+  Swal.fire({
+    padding: 0,
+  })
+  assert.equal(Swal.getPopup().style.padding, '0px')
+})
+
+QUnit.test('padding should allow a number', (assert) => {
+  Swal.fire({
+    padding: 15,
+  })
+  assert.equal(Swal.getPopup().style.padding, '15px')
+})
+
+QUnit.test('padding should allow a string', (assert) => {
+  Swal.fire({
+    padding: '2rem',
+  })
+  assert.equal(Swal.getPopup().style.padding, '2rem')
+})
+
+QUnit.test('padding should be empty with undefined', (assert) => {
+  Swal.fire({
+    padding: undefined,
+  })
+  assert.equal(Swal.getPopup().style.padding, '');
+})
+
+QUnit.test('padding should be empty with an object', (assert) => {
+  Swal.fire({
+    padding: {},
+  })
+  assert.equal(Swal.getPopup().style.padding, '');
+})
+
+QUnit.test('padding should be empty with an array', (assert) => {
+  Swal.fire({
+    padding: [],
+  })
+  assert.equal(Swal.getPopup().style.padding, '');
+})
+
+QUnit.test('padding should be empty with `true`', (assert) => {
+  Swal.fire({
+    padding: true,
+  })
+  assert.equal(Swal.getPopup().style.padding, '');
+})

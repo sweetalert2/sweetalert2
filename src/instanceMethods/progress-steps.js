87934e0ca62d8ebd419153b1b22e96a68b1fafae
Limon Monte <limon.monte@gmail.com>
2019-03-30 20:02:14 +0200

    chore: deprecate getProgressSteps() and setProgressSteps() (#1506)

--- src/instanceMethods/progress-steps.js
+++ src/instanceMethods/progress-steps.js
@@ -1,13 +1,16 @@
 import * as dom from '../utils/dom/index.js'
 import { renderProgressSteps } from '../utils/dom/renderers/renderProgressSteps'
 import privateProps from '../privateProps.js'
+import { warnAboutDepreation } from '../utils/utils.js';
 
 export function getProgressSteps () {
+  warnAboutDepreation('Swal.getProgressSteps()', `const swalInstance = Swal.fire({progressSteps: ['1', '2', '3']}); const progressSteps = swalInstance.params.progressSteps`)
   const innerParams = privateProps.innerParams.get(this)
   return innerParams.progressSteps
 }
 
 export function setProgressSteps (progressSteps) {
+  warnAboutDepreation('Swal.setProgressSteps()', 'Swal.update()')
   const innerParams = privateProps.innerParams.get(this)
   const updatedParams = Object.assign({}, innerParams, { progressSteps })
   privateProps.innerParams.set(this, updatedParams)

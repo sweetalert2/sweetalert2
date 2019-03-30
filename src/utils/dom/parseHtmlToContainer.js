27cfc37d9c36a6c95d582641ecb6b15d47921699
Limon Monte <limon.monte@gmail.com>
2019-03-30 23:31:02 +0200

    chore: eliminate cognitive complexity of parseHtmlToContainer()

--- src/utils/dom/parseHtmlToContainer.js
+++ src/utils/dom/parseHtmlToContainer.js
@@ -2,17 +2,24 @@ export const parseHtmlToContainer = (param, target) => {
   // DOM element
   if (param instanceof HTMLElement) {
     target.appendChild(param)
+
   // JQuery element(s)
   } else if (typeof param === 'object') {
-    target.innerHTML = ''
-    if (0 in param) {
-      for (let i = 0; i in param; i++) {
-        target.appendChild(param[i].cloneNode(true))
-      }
-    } else {
-      target.appendChild(param.cloneNode(true))
-    }
+    handleJqueryElem(target, param)
+
+  // Plain string
   } else if (param) {
     target.innerHTML = param
+  }
+}
+
+const handleJqueryElem = (target, elem) => {
+  target.innerHTML = ''
+  if (0 in elem) {
+    for (let i = 0; i in elem; i++) {
+      target.appendChild(elem[i].cloneNode(true))
+    }
+  } else {
+    target.appendChild(elem.cloneNode(true))
   }
 }

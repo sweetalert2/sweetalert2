048dadedd5dddab035e2c35ce335a7b60f53808d
Limon Monte <limon.monte@gmail.com>
2019-04-01 15:04:09 +0300

    chore: eliminate cognitive complexity of restoreActiveElement()

--- src/globalState.js
+++ src/globalState.js
@@ -4,18 +4,22 @@ const globalState = {}
 
 export default globalState
 
+const focusPreviousActiveElement = () => {
+  if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
+    globalState.previousActiveElement.focus()
+    globalState.previousActiveElement = null
+  } else if (document.body) {
+    document.body.focus()
+  }
+}
+
 // Restore previous active (focused) element
 export const restoreActiveElement = () => {
   return new Promise(resolve => {
     const x = window.scrollX
     const y = window.scrollY
     globalState.restoreFocusTimeout = setTimeout(() => {
-      if (globalState.previousActiveElement && globalState.previousActiveElement.focus) {
-        globalState.previousActiveElement.focus()
-        globalState.previousActiveElement = null
-      } else if (document.body) {
-        document.body.focus()
-      }
+      focusPreviousActiveElement()
       resolve()
     }, RESTORE_FOCUS_TIMEOUT) // issues/900
     if (typeof x !== 'undefined' && typeof y !== 'undefined') { // IE doesn't have scrollX/scrollY support

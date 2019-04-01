9af67f0fe3ff26de6bf0296ffc3f210c77ab7cff
Limon Monte <limon.monte@gmail.com>
2019-04-01 16:18:05 +0300

    chore: eliminate cognitive complexity of restoreProgressSteps() (#1513)

--- src/utils/dom/renderers/renderProgressSteps.js
+++ src/utils/dom/renderers/renderProgressSteps.js
@@ -3,36 +3,48 @@ import { warn } from '../../utils.js'
 import * as dom from '../../dom/index.js'
 import sweetAlert from '../../../sweetalert2.js'
 
+const createStepElement = (step) => {
+  const stepEl = document.createElement('li')
+  dom.addClass(stepEl, swalClasses['progress-step'])
+  stepEl.innerHTML = step
+  return stepEl
+}
+
+const createLineElement = (params) => {
+  let lineEl = document.createElement('li')
+  dom.addClass(lineEl, swalClasses['progress-step-line'])
+  if (params.progressStepsDistance) {
+    lineEl.style.width = params.progressStepsDistance
+  }
+  return lineEl
+}
+
 export const renderProgressSteps = (params) => {
   let progressStepsContainer = dom.getProgressSteps()
-  let currentProgressStep = parseInt(params.currentProgressStep === null ? sweetAlert.getQueueStep() : params.currentProgressStep, 10)
-  if (params.progressSteps && params.progressSteps.length) {
-    dom.show(progressStepsContainer)
-    progressStepsContainer.innerHTML = ''
-    if (currentProgressStep >= params.progressSteps.length) {
-      warn(
-        'Invalid currentProgressStep parameter, it should be less than progressSteps.length ' +
-        '(currentProgressStep like JS arrays starts from 0)'
-      )
-    }
-    params.progressSteps.forEach((step, index) => {
-      let stepEl = document.createElement('li')
-      dom.addClass(stepEl, swalClasses['progress-step'])
-      stepEl.innerHTML = step
-      if (index === currentProgressStep) {
-        dom.addClass(stepEl, swalClasses['active-progress-step'])
-      }
-      progressStepsContainer.appendChild(stepEl)
-      if (index !== params.progressSteps.length - 1) {
-        let line = document.createElement('li')
-        dom.addClass(line, swalClasses['progress-step-line'])
-        if (params.progressStepsDistance) {
-          line.style.width = params.progressStepsDistance
-        }
-        progressStepsContainer.appendChild(line)
-      }
-    })
-  } else {
-    dom.hide(progressStepsContainer)
+  if (!params.progressSteps || params.progressSteps.length === 0) {
+    return dom.hide(progressStepsContainer)
   }
+
+  dom.show(progressStepsContainer)
+  progressStepsContainer.innerHTML = ''
+  const currentProgressStep = parseInt(params.currentProgressStep === null ? sweetAlert.getQueueStep() : params.currentProgressStep)
+  if (currentProgressStep >= params.progressSteps.length) {
+    warn(
+      'Invalid currentProgressStep parameter, it should be less than progressSteps.length ' +
+      '(currentProgressStep like JS arrays starts from 0)'
+    )
+  }
+
+  params.progressSteps.forEach((step, index) => {
+    const stepEl = createStepElement(step)
+    progressStepsContainer.appendChild(stepEl)
+    if (index === currentProgressStep) {
+      dom.addClass(stepEl, swalClasses['active-progress-step'])
+    }
+
+    if (index !== params.progressSteps.length - 1) {
+      const lineEl = createLineElement(step, index)
+      progressStepsContainer.appendChild(lineEl)
+    }
+  })
 }

adcfdceb3f4b67a3354c3d4fcb63230802d6afd2
Limon Monte <limonte@users.noreply.github.com>
2019-04-02 16:51:32 +0300

    chore: move methods of the Timer class outside of the constructor (#1516)

--- src/utils/Timer.js
+++ src/utils/Timer.js
@@ -1,50 +1,51 @@
 export default class Timer {
   constructor (callback, delay) {
-    let id, started, remaining = delay
+    this.callback = callback
+    this.remaining = delay
     this.running = false
 
-    this.start = function () {
-      if (!this.running) {
-        this.running = true
-        started = new Date()
-        id = setTimeout(callback, remaining)
-      }
-      return remaining
-    }
-
-    this.stop = function () {
-      if (this.running) {
-        this.running = false
-        clearTimeout(id)
-        remaining -= new Date() - started
-      }
-      return remaining
-    }
-
-    this.increase = function (n) {
-      const running = this.running
-      if (running) {
-        this.stop()
-      }
-      remaining += n
-      if (running) {
-        this.start()
-      }
-      return remaining
-    }
-
-    this.getTimerLeft = function () {
-      if (this.running) {
-        this.stop()
-        this.start()
-      }
-      return remaining
-    }
-
-    this.isRunning = function () {
-      return this.running
-    }
-
     this.start()
+  }
+
+  start() {
+    if (!this.running) {
+      this.running = true
+      this.started = new Date()
+      this.id = setTimeout(this.callback, this.remaining)
+    }
+    return this.remaining
+  }
+
+  stop() {
+    if (this.running) {
+      this.running = false
+      clearTimeout(this.id)
+      this.remaining -= new Date() - this.started
+    }
+    return this.remaining
+  }
+
+  increase(n) {
+    const running = this.running
+    if (running) {
+      this.stop()
+    }
+    this.remaining += n
+    if (running) {
+      this.start()
+    }
+    return this.remaining
+  }
+
+  getTimerLeft() {
+    if (this.running) {
+      this.stop()
+      this.start()
+    }
+    return this.remaining
+  }
+
+  isRunning() {
+    return this.running
   }
 }

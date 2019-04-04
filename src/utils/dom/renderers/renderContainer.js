b4e015f178b89f706de0a370201cf9ecbb711234
gverni <g.verni@gmail.com>
2019-04-04 17:01:46 +0100

    Reduced complexity of renderContainer
    Created 3 functions:
    * handleBackdropParam()
    * handlePositionParam()
    * handleGrowParam()

--- src/utils/dom/renderers/renderContainer.js
+++ src/utils/dom/renderers/renderContainer.js
@@ -2,6 +2,32 @@ import { swalClasses } from '../../classes.js'
 import { warn } from '../../utils.js'
 import * as dom from '../../dom/index.js'
 
+function handleBackdropParam (container, backdrop) {
+  if (typeof backdrop === 'string') {
+    container.style.background = backdrop
+  } else if (!backdrop) {
+    dom.addClass([document.documentElement, document.body], swalClasses['no-backdrop'])
+  }
+}
+
+function handlePositionParam (container, position) {
+  if (position in swalClasses) {
+    dom.addClass(container, swalClasses[position])
+  } else {
+    warn('The "position" parameter is not valid, defaulting to "center"')
+    dom.addClass(container, swalClasses.center)
+  }
+}
+
+function handleGrowParam (container, grow) {
+  if (grow && typeof grow === 'string') {
+    let growClass = 'grow-' + grow
+    if (growClass in swalClasses) {
+      dom.addClass(container, swalClasses[growClass])
+    }
+  }
+}
+
 export const renderContainer = (params) => {
   const container = dom.getContainer()
 
@@ -9,31 +35,14 @@ export const renderContainer = (params) => {
     return
   }
 
-  // Backdrop
-  if (typeof params.backdrop === 'string') {
-    container.style.background = params.backdrop
-  } else if (!params.backdrop) {
-    dom.addClass([document.documentElement, document.body], swalClasses['no-backdrop'])
-  }
+  handleBackdropParam(container, params.backdrop)
+
   if (!params.backdrop && params.allowOutsideClick) {
     warn('"allowOutsideClick" parameter requires `backdrop` parameter to be set to `true`')
   }
 
-  // Position
-  if (params.position in swalClasses) {
-    dom.addClass(container, swalClasses[params.position])
-  } else {
-    warn('The "position" parameter is not valid, defaulting to "center"')
-    dom.addClass(container, swalClasses.center)
-  }
-
-  // Grow
-  if (params.grow && typeof params.grow === 'string') {
-    let growClass = 'grow-' + params.grow
-    if (growClass in swalClasses) {
-      dom.addClass(container, swalClasses[growClass])
-    }
-  }
+  handlePositionParam(container, params.position)
+  handleGrowParam(container, params.grow)
 
   // Custom class
   dom.applyCustomClass(container, params.customClass, 'container')

19d0ac57dcef3302e2a863105f785b5be32f4771
Limon Monte <limonte@users.noreply.github.com>
2019-05-06 12:49:19 +0300

    chore: improve jsdelivr cache purge

--- tools/purge-jsdelivr.js
+++ tools/purge-jsdelivr.js
@@ -6,18 +6,17 @@ const log = console.log // eslint-disable-line
 ;(async () => {
   log(`Purge jsdelivr cache...`)
 
-  log(' - @latest')
-  await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@latest`, { skipLogging: true })
-  log(' - @8')
-  await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@8`, { skipLogging: true })
-  log(' - @7')
-  await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@7`, { skipLogging: true })
-
   const distFiles = fs.readdirSync('dist')
-  for (const distFile of distFiles) {
-    log(` - ${distFile}`)
-    await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@latest/dist/${distFile}`, { skipLogging: true })
-    await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@7/dist/${distFile}`, { skipLogging: true })
+
+  for (const version of ['latest', '7', '8']) {
+    log(` - @${version}`)
+    await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@${version}`, { skipLogging: true })
+
+    // dist
+    for (const distFile of distFiles) {
+      log(`   - dist/${distFile}`)
+      await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@${version}/dist/${distFile}`, { skipLogging: true })
+    }
   }
 
   log('OK!')

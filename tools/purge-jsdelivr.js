053568c934a02d81e958e6b5473407af2006bfc8
Limon Monte <limon.monte@gmail.com>
2019-05-07 10:41:09 +0300

    chore: bump dev-deps

--- tools/purge-jsdelivr.js
+++ tools/purge-jsdelivr.js
@@ -8,7 +8,7 @@ const log = console.log // eslint-disable-line
 
   const distFiles = fs.readdirSync('dist')
 
-  for (const version of ['latest', '7', '8']) {
+  for (const version of ['8', 'latest']) {
     log(` - @${version}`)
     await execute(`curl --silent https://purge.jsdelivr.net/npm/sweetalert2@${version}`, { skipLogging: true })
 

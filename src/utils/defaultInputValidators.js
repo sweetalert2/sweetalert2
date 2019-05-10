7f7805de2a1dca8445c5cbd3d0904503c734f7e6
Limon Monte <limon.monte@gmail.com>
2019-05-10 13:14:26 +0300

    chore: remove duplicated / character from url regex

--- src/utils/defaultInputValidators.js
+++ src/utils/defaultInputValidators.js
@@ -6,7 +6,7 @@ export default {
   },
   url: (string, validationMessage) => {
     // taken from https://stackoverflow.com/a/3809435 with a small change from #1306
-    return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/.test(string)
+    return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$/.test(string)
       ? Promise.resolve()
       : Promise.resolve(validationMessage ? validationMessage : 'Invalid URL')
   }

f310501a5cc0c61a2b339143ee5d82925eeaeba9
gverni <g.verni@gmail.com>
2019-07-04 16:29:59 +0100

    chore: reduce method complexity of setParameters() (#1634)
    * Reduce method complexity of setParameters()
    
    Created two functions:
    * setDefaultInputValidators(): to assign default input  validators
    * validateCustomTargetElement(): to check if the target element
    is a valid one
    
    Also:
    * Moved warning message to a constant outside of function
    * Code clean-up
    
    * Force dom.init() at the end of setParameters()
    
    The previous logic was redundant because was returning always true
    
    * no need in the loaderOnConfirmMessage variable

--- src/utils/setParameters.js
+++ src/utils/setParameters.js
@@ -2,13 +2,7 @@ import { warn, callIfFunction } from './utils.js'
 import * as dom from './dom/index.js'
 import defaultInputValidators from './defaultInputValidators.js'
 
-/**
- * Set type, text and actions on popup
- *
- * @param params
- * @returns {boolean}
- */
-export default function setParameters (params) {
+function setDefaultInputValidators (params) {
   // Use default `inputValidator` for supported input types if not provided
   if (!params.inputValidator) {
     Object.keys(defaultInputValidators).forEach((key) => {
@@ -17,6 +11,28 @@ export default function setParameters (params) {
       }
     })
   }
+}
+
+function validateCustomTargetElement (params) {
+  // Determine if the custom target element is valid
+  if (
+    !params.target ||
+    (typeof params.target === 'string' && !document.querySelector(params.target)) ||
+    (typeof params.target !== 'string' && !params.target.appendChild)
+  ) {
+    warn('Target parameter is not valid, defaulting to "body"')
+    params.target = 'body'
+  }
+}
+
+/**
+ * Set type, text and actions on popup
+ *
+ * @param params
+ * @returns {boolean}
+ */
+export default function setParameters (params) {
+  setDefaultInputValidators(params)
 
   // showLoaderOnConfirm && preConfirm
   if (params.showLoaderOnConfirm && !params.preConfirm) {
@@ -33,29 +49,12 @@ export default function setParameters (params) {
   // inside the params.animation function
   params.animation = callIfFunction(params.animation)
 
-  // Determine if the custom target element is valid
-  if (
-    !params.target ||
-    (typeof params.target === 'string' && !document.querySelector(params.target)) ||
-    (typeof params.target !== 'string' && !params.target.appendChild)
-  ) {
-    warn('Target parameter is not valid, defaulting to "body"')
-    params.target = 'body'
-  }
+  validateCustomTargetElement(params)
 
   // Replace newlines with <br> in title
   if (typeof params.title === 'string') {
-    params.title = params.title.split('\n').join('<br />');
+    params.title = params.title.split('\n').join('<br />')
   }
 
-  const oldPopup = dom.getPopup()
-  let targetElement = typeof params.target === 'string' ? document.querySelector(params.target) : params.target
-  if (
-    !oldPopup ||
-    // If the model target has changed, refresh the popup
-    (oldPopup && targetElement && oldPopup.parentNode !== targetElement.parentNode)
-  ) {
-    dom.init(params)
-  }
+  dom.init(params)
 }
-

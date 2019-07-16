edb8cf90a6ffb9ea2d5ef3744369471ef2c4a1b0
kwdowik <wdowikkacper@gmail.com>
2019-07-17 00:37:16 +0200

    Fix openPopup method complexity

--- src/utils/openPopup.js
+++ src/utils/openPopup.js
@@ -20,48 +20,60 @@ export const openPopup = (params) => {
   const container = dom.getContainer()
   const popup = dom.getPopup()
 
-  if (params.onBeforeOpen !== null && typeof params.onBeforeOpen === 'function') {
+  if (typeof params.onBeforeOpen === 'function') {
     params.onBeforeOpen(popup)
   }
 
-  if (params.animation) {
-    dom.addClass(popup, swalClasses.show)
-    dom.addClass(container, swalClasses.fade)
-  }
-  dom.show(popup)
+  addClasses({ container, popup })(params)
 
   // scrolling is 'hidden' until animation is done, after that 'auto'
+  setScrollingVisibility({ container, popup })
+
+  if (dom.isModal()) {
+    fixScrollContainer(container)(params.scrollbarPadding)
+  }
+
+  if (!dom.isToast() && !globalState.previousActiveElement) {
+    globalState.previousActiveElement = document.activeElement
+  }
+  if (typeof params.onOpen === 'function') {
+    setTimeout(() => params.onOpen(popup))
+  }
+}
+
+const setScrollingVisibility = ({ container, popup }) => {
   if (dom.animationEndEvent && dom.hasCssAnimation(popup)) {
     container.style.overflowY = 'hidden'
     popup.addEventListener(dom.animationEndEvent, swalOpenAnimationFinished.bind(null, popup, container))
   } else {
     container.style.overflowY = 'auto'
   }
+}
+
+const fixScrollContainer = (container) => (scrollbarPadding) => {
+  iOSfix()
+  IEfix()
+  setAriaHidden()
+
+  if (scrollbarPadding) {
+    fixScrollbar()
+  }
+
+  // sweetalert2/issues/1247
+  setTimeout(() => {
+    container.scrollTop = 0
+  })
+}
+
+const addClasses = ({ container, popup }) => (params) => {
+  if (params.animation) {
+    dom.addClass(popup, swalClasses.show)
+    dom.addClass(container, swalClasses.fade)
+  }
+  dom.show(popup)
 
   dom.addClass([document.documentElement, document.body, container], swalClasses.shown)
   if (params.heightAuto && params.backdrop && !params.toast) {
     dom.addClass([document.documentElement, document.body], swalClasses['height-auto'])
-  }
-
-  if (dom.isModal()) {
-    if (params.scrollbarPadding) {
-      fixScrollbar()
-    }
-    iOSfix()
-    IEfix()
-    setAriaHidden()
-
-    // sweetalert2/issues/1247
-    setTimeout(() => {
-      container.scrollTop = 0
-    })
-  }
-  if (!dom.isToast() && !globalState.previousActiveElement) {
-    globalState.previousActiveElement = document.activeElement
-  }
-  if (params.onOpen !== null && typeof params.onOpen === 'function') {
-    setTimeout(() => {
-      params.onOpen(popup)
-    })
   }
 }

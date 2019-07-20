a5d81647a7715c942687e14afc2370fd0b184e9d
Limon Monte <limonte@users.noreply.github.com>
2019-07-20 20:47:36 +0300

    chore: extract handlePopupClick() (#1680)

--- src/instanceMethods/_main.js
+++ src/instanceMethods/_main.js
@@ -11,6 +11,7 @@ import privateMethods from '../privateMethods.js'
 import { handleInputOptions, handleInputValue } from '../utils/dom/inputUtils.js'
 import { handleConfirmButtonClick, handleCancelButtonClick } from './buttons-handlers.js'
 import { addKeydownHandler, setFocus } from './keydown-handler.js'
+import { handlePopupClick } from './popup-click-handler.js'
 import { DismissReason } from '../utils/DismissReason.js'
 
 export function _main (userParams) {
@@ -82,59 +83,7 @@ const swalPromise = (instance, domCache, innerParams) => {
       dismissWith(DismissReason.close)
     }
 
-    if (innerParams.toast) {
-      // Closing popup by internal click
-      domCache.popup.onclick = () => {
-        if (
-          innerParams.showConfirmButton ||
-          innerParams.showCancelButton ||
-          innerParams.showCloseButton ||
-          innerParams.input
-        ) {
-          return
-        }
-        dismissWith(DismissReason.close)
-      }
-    } else {
-      let ignoreOutsideClick = false
-
-      // Ignore click events that had mousedown on the popup but mouseup on the container
-      // This can happen when the user drags a slider
-      domCache.popup.onmousedown = () => {
-        domCache.container.onmouseup = function (e) {
-          domCache.container.onmouseup = undefined
-          // We only check if the mouseup target is the container because usually it doesn't
-          // have any other direct children aside of the popup
-          if (e.target === domCache.container) {
-            ignoreOutsideClick = true
-          }
-        }
-      }
-
-      // Ignore click events that had mousedown on the container but mouseup on the popup
-      domCache.container.onmousedown = () => {
-        domCache.popup.onmouseup = function (e) {
-          domCache.popup.onmouseup = undefined
-          // We also need to check if the mouseup target is a child of the popup
-          if (e.target === domCache.popup || domCache.popup.contains(e.target)) {
-            ignoreOutsideClick = true
-          }
-        }
-      }
-
-      domCache.container.onclick = (e) => {
-        if (ignoreOutsideClick) {
-          ignoreOutsideClick = false
-          return
-        }
-        if (e.target !== domCache.container) {
-          return
-        }
-        if (callIfFunction(innerParams.allowOutsideClick)) {
-          dismissWith(DismissReason.backdrop)
-        }
-      }
-    }
+    handlePopupClick(domCache, innerParams, dismissWith)
 
     // Reverse buttons (Confirm on the right side)
     if (innerParams.reverseButtons) {

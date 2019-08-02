80bd6d81e2c472dba68725f8a0ce5ac13ae82ba9
Limon Monte <limon.monte@gmail.com>
2019-08-02 14:54:03 +0300

    chore: make eslint happy

--- sweetalert2.d.ts
+++ sweetalert2.d.ts
@@ -903,15 +903,15 @@ declare module 'sweetalert2' {
 }
 
 declare module 'sweetalert2/*/sweetalert2.js' {
-  export * from 'sweetalert2';
+  export * from 'sweetalert2'
   // "export *" does not matches the default export, so do it explicitly.
-  export { default } from 'sweetalert2';
+  export { default } from 'sweetalert2' // eslint-disable-line
 }
 
 declare module 'sweetalert2/*/sweetalert2.all.js' {
-  export * from 'sweetalert2';
+  export * from 'sweetalert2'
   // "export *" does not matches the default export, so do it explicitly.
-  export { default } from 'sweetalert2';
+  export { default } from 'sweetalert2' // eslint-disable-line
 }
 
 /**

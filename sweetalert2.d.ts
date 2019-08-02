463423b59694b803746cc9299a31cfd9c3ed7c96
Limon Monte <limon.monte@gmail.com>
2019-08-02 14:28:04 +0300

    fix(types): support sweetalert2 modules in dist and src folders
    based on https://stackoverflow.com/a/57323500

--- sweetalert2.d.ts
+++ sweetalert2.d.ts
@@ -902,6 +902,18 @@ declare module 'sweetalert2' {
   export default Swal
 }
 
+declare module 'sweetalert2/*/sweetalert2.js' {
+  export * from 'sweetalert2';
+  // "export *" does not matches the default export, so do it explicitly.
+  export { default } from 'sweetalert2';
+}
+
+declare module 'sweetalert2/*/sweetalert2.all.js' {
+  export * from 'sweetalert2';
+  // "export *" does not matches the default export, so do it explicitly.
+  export { default } from 'sweetalert2';
+}
+
 /**
  * These interfaces aren't provided by SweetAlert2, but its definitions use them.
  * They will be merged with 'true' definitions.

2ac9167537b1cb74fd8376e5d49ffe2e7d790f27
Limon Monte <limonte@users.noreply.github.com>
2019-08-20 01:15:49 +0300

    chore: remove parserOptions, it's in eslint-config-standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,6 @@ module.exports = {
   plugins: [
     'import'
   ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/indent': ['error', 2],

53d91066647d13ff71634d976c904046ac22f8cc
Limon Monte <limon.monte@gmail.com>
2019-11-04 12:16:48 +0200

    BREAKING CHANGE: remove setProgressSteps(), showProgressSteps(), hideProgressSteps()
    BREAKING CHANGE: remove setProgressSteps(), showProgressSteps(), hideProgressSteps()
    
    Closes #1507
    Closes #1673

--- test/qunit/methods/getProgressSteps.js
+++
@@ -1,7 +0,0 @@
-import { Swal } from '../helpers.js'
-
-QUnit.test('getProgressSteps() method', (assert) => {
-  Swal.fire({ progressSteps: ['1', '2', '3'] })
-  const progressSteps = Swal.getProgressSteps()
-  assert.deepEqual(progressSteps, ['1', '2', '3'])
-})

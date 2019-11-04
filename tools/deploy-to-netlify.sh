c5905421e903b78629621d874d8d71ce39e11422
Limon Monte <limon.monte@gmail.com>
2019-11-05 00:22:15 +0200

    chore(readme): add link to v9 release notes

--- tools/deploy-to-netlify.sh
+++ tools/deploy-to-netlify.sh
@@ -3,5 +3,5 @@
 yarn build
 curl https://sweetalert2.github.io -o index.html
 sed -i 's|src="/|src="https://sweetalert2.github.io/|g' index.html
-sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@8/||g' index.html
+sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@9/||g' index.html
 sed -i 's|="./|="https://sweetalert2.github.io/|g' index.html

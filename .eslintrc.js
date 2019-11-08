014cf2bb6549e755df86fa97ac4cb505e2e07e39
Limon Monte <limonte@users.noreply.github.com>
2019-11-08 18:15:18 +0200

    chore: use @sweetalert2/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,8 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    node: true,
-    commonjs: true,
-    es6: true
-  },
   extends: [
-    'standard',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: [
-    'import'
+    '@sweetalert2/eslint-config'
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/no-this-alias': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-empty-interface': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    'comma-dangle': 0,
-    'complexity': ['error', { max: 10 }],
     'import/extensions': ['error', 'always'],
-    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
-    'no-var': ['error'],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
   }
 }

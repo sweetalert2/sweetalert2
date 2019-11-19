85fad06933f9e1edf8c4fcfa5ffc41a7b53453be
Limon Monte <limonte@users.noreply.github.com>
2019-11-19 13:33:46 +0200

    fix: hasClass multiple classes support for IE11

---
+++ test/qunit/params/showClass.js
@@ -0,0 +1,20 @@
+const { Swal } = require('../helpers')
+
+QUnit.test('showClass + hideClass', (assert) => {
+  const done = assert.async()
+
+  Swal.fire({
+    title: 'Custom animation with Animate.css',
+    showClass: {
+      popup: 'animated fadeInDown faster'
+    },
+    hideClass: {
+      popup: 'animated fadeOutUp faster'
+    },
+    onAfterClose: () => {
+      assert.notOk(Swal.isVisible())
+      done()
+    }
+  })
+  Swal.close()
+})

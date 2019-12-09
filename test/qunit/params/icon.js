b3178146cb6b277540ebdaabccfcd283faaac892
Limon Monte <limon.monte@gmail.com>
2019-12-10 00:45:48 +0200

    fix: make icon classes op popup different from classes on icons

--- test/qunit/params/icon.js
+++ test/qunit/params/icon.js
@@ -1,9 +1,9 @@
 const { Swal, SwalWithoutAnimation } = require('../helpers')
-const { iconTypes } = require('../../../src/utils/classes')
+const { iconTypes, swalClasses } = require('../../../src/utils/classes')
 
 QUnit.test('The popup should have the icon class', (assert) => {
   for (const icon in iconTypes) {
     SwalWithoutAnimation.fire({ icon })
-    assert.ok(Swal.getPopup().classList.contains(iconTypes[icon]))
+    assert.ok(Swal.getPopup().classList.contains(swalClasses[`icon-${icon}`]))
   }
 })

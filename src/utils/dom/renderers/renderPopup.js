7922360163d76ebff7529e244be874493afccc9c
Limon Monte <limon.monte@gmail.com>
2019-12-10 01:26:29 +0200

    fix: do not re-add popup's showClass

--- src/utils/dom/renderers/renderPopup.js
+++ src/utils/dom/renderers/renderPopup.js
@@ -20,8 +20,9 @@ export const renderPopup = (instance, params) => {
 }
 
 const addClasses = (popup, params) => {
-  // Default Class
-  popup.className = swalClasses.popup
+  // Default Class + showClass when updating Swal.update({})
+  popup.className = `${swalClasses.popup} ${dom.isVisible(popup) ? params.showClass.popup : ''}`
+
   if (params.toast) {
     dom.addClass([document.documentElement, document.body], swalClasses['toast-shown'])
     dom.addClass(popup, swalClasses.toast)
@@ -38,10 +39,5 @@ const addClasses = (popup, params) => {
   // Icon class (#1842)
   if (params.icon) {
     dom.addClass(popup, swalClasses[`icon-${params.icon}`])
-  }
-
-  // Add showClass when updating Swal.update({})
-  if (dom.isVisible(popup)) {
-    dom.addClass(popup, params.showClass.popup)
   }
 }

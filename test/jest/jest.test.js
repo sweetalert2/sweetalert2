cbb7819fa04c7f5150b1b9e204c356d58c318a3a
Limon Monte <limon.monte@gmail.com>
2019-12-16 12:06:35 +0200

    chore: bump yarn.lock

--- test/jest/jest.test.js
+++ test/jest/jest.test.js
@@ -3,6 +3,7 @@ const Swal = require('../../dist/sweetalert2')
 describe('sweetalert2', () => {
   beforeAll(() => {
     // jest (or more precisely, jsdom) doesn't implement `window.scrollTo` so we need to mock it
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     window.scrollTo = () => {}
   })
 

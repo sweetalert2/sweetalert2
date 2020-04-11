ebaf1799804a8745fe463b52d98a02326b1e6fb8
Limon Monte <limonte@users.noreply.github.com>
2020-04-11 23:09:40 +0300

    chore: bump @cypress/code-coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on) => {
-  on('task', require('@cypress/code-coverage/task'))
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
+  return config
 }

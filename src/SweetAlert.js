108171bbf024c3251e3f905fb6354f17bd4dacb1
Limon Monte <limon.monte@gmail.com>
2020-04-19 02:48:07 -0700

    chore: convert to es6 class (#1951)

--- src/SweetAlert.js
+++ src/SweetAlert.js
@@ -6,45 +6,45 @@ import privateProps from './privateProps.js'
 
 let currentInstance
 
-// SweetAlert constructor
-function SweetAlert (...args) {
-  // Prevent run in Node env
-  /* istanbul ignore if */
-  if (typeof window === 'undefined') {
-    return
-  }
-
-  // Check for the existence of Promise
-  /* istanbul ignore if */
-  if (typeof Promise === 'undefined') {
-    error('This package requires a Promise library, please include a shim to enable it in this browser (See: https://github.com/sweetalert2/sweetalert2/wiki/Migration-from-SweetAlert-to-SweetAlert2#1-ie-support)')
-  }
-
-  currentInstance = this
-
-  const outerParams = Object.freeze(this.constructor.argsToParams(args))
-
-  Object.defineProperties(this, {
-    params: {
-      value: outerParams,
-      writable: false,
-      enumerable: true,
-      configurable: true
+class SweetAlert {
+  constructor (...args) {
+    // Prevent run in Node env
+    if (typeof window === 'undefined') {
+      return
     }
-  })
 
-  const promise = this._main(this.params)
-  privateProps.promise.set(this, promise)
-}
+    // Check for the existence of Promise
+    if (typeof Promise === 'undefined') {
+      error('This package requires a Promise library, please include a shim to enable it in this browser (See: https://github.com/sweetalert2/sweetalert2/wiki/Migration-from-SweetAlert-to-SweetAlert2#1-ie-support)')
+    }
 
-// `catch` cannot be the name of a module export, so we define our thenable methods here instead
-SweetAlert.prototype.then = function (onFulfilled) {
-  const promise = privateProps.promise.get(this)
-  return promise.then(onFulfilled)
-}
-SweetAlert.prototype.finally = function (onFinally) {
-  const promise = privateProps.promise.get(this)
-  return promise.finally(onFinally)
+    currentInstance = this
+
+    const outerParams = Object.freeze(this.constructor.argsToParams(args))
+
+    Object.defineProperties(this, {
+      params: {
+        value: outerParams,
+        writable: false,
+        enumerable: true,
+        configurable: true
+      }
+    })
+
+    const promise = this._main(this.params)
+    privateProps.promise.set(this, promise)
+  }
+
+  // `catch` cannot be the name of a module export, so we define our thenable methods here instead
+  then (onFulfilled) {
+    const promise = privateProps.promise.get(this)
+    return promise.then(onFulfilled)
+  }
+
+  finally (onFinally) {
+    const promise = privateProps.promise.get(this)
+    return promise.finally(onFinally)
+  }
 }
 
 // Assign instance methods from src/instanceMethods/*.js to prototype

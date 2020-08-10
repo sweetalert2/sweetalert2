a2de1e054e08be31adbfd22c02157a6046a5c6c1
Limon Monte <limon.monte@gmail.com>
2020-08-10 12:45:21 +0300

    chore: improve Swal.getTimerLeft() test (#2031)

--- test/qunit/methods/getTimerLeft.js
+++ test/qunit/methods/getTimerLeft.js
@@ -1,12 +1,17 @@
 import { Swal } from '../helpers.js'
 
 QUnit.test('getTimerLeft() method', (assert) => {
+  const done = assert.async()
+
   Swal.fire({
     timer: 1000
   })
-  const timerLeft = Swal.getTimerLeft()
-  assert.ok(timerLeft > 0)
-  assert.ok(timerLeft <= 1000)
+  setTimeout(() => {
+    const timerLeft = Swal.getTimerLeft()
+    assert.ok(timerLeft > 0)
+    assert.ok(timerLeft < 1000)
+    done()
+  }, 1)
 })
 
 QUnit.test('getTimerLeft() should return null if popup does not have timer', (assert) => {

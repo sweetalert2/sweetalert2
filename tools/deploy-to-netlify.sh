6fcf16ec068f37cbddf9c34f46e448846d4895df
Limon Monte <limonte@users.noreply.github.com>
2020-09-22 02:11:55 +0300

    fix: add the opacity workaround and timeout for animating popup (#2060)
    * add timeout for showing and animating popup
    
    * fix deploy to netlify
    
    * add workaround with opacity when adding showClass
    
    * fix test

--- tools/deploy-to-netlify.sh
+++ tools/deploy-to-netlify.sh
@@ -5,3 +5,4 @@ curl https://sweetalert2.github.io -o index.html
 sed -i 's|src="/|src="https://sweetalert2.github.io/|g' index.html
 sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@10/||g' index.html
 sed -i 's|="./|="https://sweetalert2.github.io/|g' index.html
+sed -i 's|="/|="https://sweetalert2.github.io/|g' index.html

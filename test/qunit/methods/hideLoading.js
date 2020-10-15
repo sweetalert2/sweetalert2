366a670e6f1eabb3cc45141e45f8a135b9688b3e
Limon Monte <limon.monte@gmail.com>
2020-10-15 23:28:58 +0300

    fix: set display: inline block for confirm button

--- test/qunit/methods/hideLoading.js
+++ test/qunit/methods/hideLoading.js
@@ -1,4 +1,10 @@
-import { Swal, ensureClosed } from '../helpers.js'
+import { Swal, ensureClosed, SwalWithoutAnimation } from '../helpers.js'
+
+QUnit.test('hideLoading should set display: inline-block to confirmButton', (assert) => {
+  SwalWithoutAnimation.fire()
+  Swal.hideLoading()
+  assert.equal(Swal.getConfirmButton().style.display, 'inline-block')
+})
 
 QUnit.test('hideLoading should not fail with closed popup', (assert) => {
   ensureClosed()

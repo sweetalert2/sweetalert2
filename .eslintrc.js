35fcdfaae996f787e6cd1199f50595d65f8b673a
Limon Monte <limon.monte@gmail.com>
2020-11-27 01:05:08 +0200

    fix: prevent setting innerHTML which is unsafe (#2116)
    * add eslint-plugin-no-unsanitized
    
    * use dom.setInnerHtml()

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
-    '@sweetalert2/eslint-config'
+    '@sweetalert2/eslint-config',
+    'plugin:no-unsanitized/DOM'
   ],
   rules: {
     'import/extensions': ['error', 'always'],

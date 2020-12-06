5b9ec6f098669e5fba2c7fbf0da71cf3df859bd8
Limon Monte <limon.monte@protonmail.com>
2020-12-06 12:39:08 +0200

    make it simple

--- src/staticMethods/bindClickHandler.js
+++ src/staticMethods/bindClickHandler.js
@@ -11,26 +11,15 @@ export function bindClickHandler (attr = 'data-swal-template') {
 }
 
 const bodyClickListener = (event) => {
-  let el = event.target
-  while (1) {
-    // using .parentNode instead of event.path because of better support by old browsers
-    // https://stackoverflow.com/a/39245638
-    el = checkElementAndGetItsParentNode(el)
-    if (!el || !el.getAttribute) { // window.getAttribute is undefined
-      break
+  // 1. using .parentNode instead of event.path because of better support by old browsers https://stackoverflow.com/a/39245638
+  // 2. using .parentNode instead of .parentElement because of IE11 + SVG https://stackoverflow.com/a/36270354
+  for (let el = event.target; el.parentNode !== document; el = el.parentNode) {
+    for (const attr in clickHandlers) {
+      const template = el.getAttribute(attr)
+      if (template) {
+        clickHandlers[attr].fire({ template })
+        return
+      }
     }
   }
-}
-
-const checkElementAndGetItsParentNode = (el) => {
-  for (const attr in clickHandlers) {
-    const template = el.getAttribute(attr)
-    if (template) {
-      clickHandlers[attr].fire({ template })
-      return false
-    }
-  }
-  // using .parentNode instead of .parentElement because of IE11 + SVG
-  // https://stackoverflow.com/a/36270354
-  return el.parentNode
 }

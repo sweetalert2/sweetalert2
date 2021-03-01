ed432251562e1bbdca901484b1032ccac871802e
Limon Monte <limonte@users.noreply.github.com>
2021-03-01 22:55:10 +0200

    Get rid of qunit tests (#2185)

--- test/qunit/methods/getTimerProgressBar.js
+++
@@ -1,9 +0,0 @@
-import { $, Swal, SwalWithoutAnimation } from '../helpers.js'
-
-QUnit.test('isTimerRunning() method', (assert) => {
-  SwalWithoutAnimation.fire({
-    timer: 500,
-    timerProgressBar: true
-  })
-  assert.equal(Swal.getTimerProgressBar(), $('.swal2-timer-progress-bar'))
-})
--- test/qunit/params/icon.js
+++
@@ -1,9 +0,0 @@
-const { Swal, SwalWithoutAnimation } = require('../helpers')
-const { iconTypes, swalClasses } = require('../../../src/utils/classes')
-
-QUnit.test('The popup should have the icon class', (assert) => {
-  for (const icon in iconTypes) {
-    SwalWithoutAnimation.fire({ icon })
-    assert.ok(Swal.getPopup().classList.contains(swalClasses[`icon-${icon}`]))
-  }
-})

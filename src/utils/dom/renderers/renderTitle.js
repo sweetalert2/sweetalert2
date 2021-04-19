27fa048d4e7b5aeb78d9622baa38704759160b38
Limon Monte <limon.monte@gmail.com>
2021-04-19 10:56:51 +0300

    fix: use display: block for popup title

--- src/utils/dom/renderers/renderTitle.js
+++ src/utils/dom/renderers/renderTitle.js
@@ -3,7 +3,7 @@ import * as dom from '../../dom/index.js'
 export const renderTitle = (instance, params) => {
   const title = dom.getTitle()
 
-  dom.toggle(title, params.title || params.titleText)
+  dom.toggle(title, params.title || params.titleText, 'block')
 
   if (params.title) {
     dom.parseHtmlToContainer(params.title, title)

590392c734c405bf0deb23084d8a603956d1e0f3
Limon Monte <limon.monte@gmail.com>
2021-04-20 12:47:39 +0300

    remove showClass/hideClass merging

--- cypress/integration/mixins.spec.js
+++ cypress/integration/mixins.spec.js
@@ -1,4 +1,5 @@
 import { Swal } from '../utils'
+import { SHOW_CLASS_TIMEOUT } from '../../src/utils/openPopup'
 
 describe('mixins', () => {
   it('basic mixin', (done) => {
@@ -39,5 +40,35 @@ describe('mixins', () => {
   it('mixins precedence', () => {
     Swal.mixin({ title: '1' }).mixin({ title: '2' }).fire()
     expect(Swal.getTitle().textContent).to.equal('2')
+  })
+
+  it('params from 2nd mixin should override params from 1st mixin', (done) => {
+    Swal
+      .mixin({ showClass: { popup: 'i-should-be-overriden' } })
+      .mixin({ showClass: { backdrop: 'backdrop-custom-show-class' } })
+      .fire({
+        didOpen: () => {
+          setTimeout(() => {
+            expect(Swal.getContainer().classList.contains('backdrop-custom-show-class')).to.be.true
+            expect(Swal.getPopup().classList.contains('i-should-be-overriden')).to.be.false
+            done()
+          }, SHOW_CLASS_TIMEOUT)
+        }
+      })
+  })
+
+  it('params from fire() should override params from mixin()', (done) => {
+    Swal
+      .mixin({ showClass: { popup: 'i-should-be-overriden' } })
+      .fire({
+        showClass: { backdrop: 'backdrop-custom-show-class' },
+        didOpen: () => {
+          setTimeout(() => {
+            expect(Swal.getContainer().classList.contains('backdrop-custom-show-class')).to.be.true
+            expect(Swal.getPopup().classList.contains('i-should-be-overriden')).to.be.false
+            done()
+          }, SHOW_CLASS_TIMEOUT)
+        }
+      })
   })
 })

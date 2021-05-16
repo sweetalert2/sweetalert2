5ad9aeb6716f5290a6c2a8dbb55cd269a48d1633
Limon Monte <limonte@users.noreply.github.com>
2021-05-16 09:29:55 +0300

    fix: footer horizontal padding

--- tools/deploy-to-netlify.sh
+++ tools/deploy-to-netlify.sh
@@ -4,6 +4,6 @@ yarn build
 curl https://sweetalert2.github.io -o index.html
 sed -i 's|"//cdn.jsdelivr|"https://cdn.jsdelivr|g' index.html
 sed -i 's|src="/|src="https://sweetalert2.github.io/|g' index.html
-sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@10/||g' index.html
+sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@11/||g' index.html
 sed -i 's|="./|="https://sweetalert2.github.io/|g' index.html
 sed -i 's|="/|="https://sweetalert2.github.io/|g' index.html

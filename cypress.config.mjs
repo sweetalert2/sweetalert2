f201dd6e76b6508b0a987d056a0ddab91a8d3c33
Limon Monte <limon.monte@gmail.com>
2022-08-05 20:30:18 +0300

    chore: bump cypress

---
+++ cypress.config.mjs
@@ -0,0 +1,9 @@
+import cypress from 'cypress'
+
+export default cypress.defineConfig({
+  e2e: {
+    supportFile: 'cypress/support/index.js',
+    video: false,
+    nodeVersion: 'bundled',
+  },
+})

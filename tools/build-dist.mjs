629ba8a794eca2b07d4cfff33f93bc52487ec4fa
Limon Monte <limonte@users.noreply.github.com>
2022-10-24 22:55:03 +0300

    fix: simplify dev environment by replacing Gulp with Vite (#2537)
    * chore(deps): update dependency eslint to v8.26.0 (#2536)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore: use vite for sandbox
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- tools/build-dist.mjs
+++ tools/build-dist.mjs
@@ -1,31 +1,23 @@
-import pify from 'pify'
-import rimraf from 'rimraf'
 import execute from '@sweetalert2/execute'
 import replaceInFile from 'replace-in-file'
 
 const log = console.log // eslint-disable-line no-console
-const removeDir = pify(rimraf)
 
-;(async () => {
-  log('Resetting the branch...')
-  await execute('git checkout .')
+log('Resetting the branch...')
+await execute('git checkout .')
 
-  log('Deleting the current dist folder...')
-  await removeDir('dist')
+// the command has been called by semantic-release, bump version in src/SweetAlert.js before building dist
+if (process.env.VERSION) {
+  log('Updating the version in src/SweetAlert.js...')
+  replaceInFile.sync({
+    files: 'src/SweetAlert.js',
+    from: /\.version = '.*?'/,
+    to: `.version = '${process.env.VERSION}'`,
+  })
+  await execute('git add src/SweetAlert.js')
+}
 
-  // the command has been called by semantic-release, bump version in src/SweetAlert.js before building dist
-  if (process.env.VERSION) {
-    log('Updating the version in src/SweetAlert.js...')
-    replaceInFile.sync({
-      files: 'src/SweetAlert.js',
-      from: /\.version = '.*?'/,
-      to: `.version = '${process.env.VERSION}'`,
-    })
-    await execute('git add src/SweetAlert.js')
-  }
+log('Running the build...')
+await execute('yarn build')
 
-  log('Running the build...')
-  await execute('yarn build')
-
-  log('OK!')
-})().catch(console.error)
+log('OK!')

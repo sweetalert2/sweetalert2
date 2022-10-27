db5f9104b82a65dd97e3370da58960b237d325d3
Limon Monte <limon.monte@gmail.com>
2022-10-27 13:09:58 +0300

    fix: add types to getInput.js

--- src/instanceMethods/getInput.js
+++ src/instanceMethods/getInput.js
@@ -3,6 +3,8 @@ import * as dom from '../utils/dom/index.js'
 
 /**
  * Gets the input DOM node, this method works with input parameter.
+ *
+ * @param {SweetAlert2} instance
  * @returns {HTMLElement | null}
  */
 export function getInput(instance) {

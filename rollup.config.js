09786c770b1c64b3ec21a92cc7342e4b1f56df75
Limon Monte <limonte@users.noreply.github.com>
2022-10-31 00:19:48 +0200

    chore: bump yarn.lock

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import { readFileSync } from 'fs'
 import babel from '@rollup/plugin-babel'
-// todo: use @rollup/plugin-terser: https://github.com/rollup/plugins/pull/1323
 import terser from '@rollup/plugin-terser'
 
 const packageJson = JSON.parse(readFileSync('package.json'))

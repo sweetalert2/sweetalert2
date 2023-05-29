af397ef435aec1afebdd2be1776b5e6448323c06
Limon Monte <limonte@users.noreply.github.com>
2023-05-29 14:00:16 +0300

    chore: strict types classes.js

--- src/utils/classes.js
+++ src/utils/classes.js
@@ -1,18 +1,87 @@
 export const swalPrefix = 'swal2-'
 
 /**
- * @param {string[]} items
- * @returns {object}
+ * @typedef
+ * { | 'container'
+ *   | 'shown'
+ *   | 'height-auto'
+ *   | 'iosfix'
+ *   | 'popup'
+ *   | 'modal'
+ *   | 'no-backdrop'
+ *   | 'no-transition'
+ *   | 'toast'
+ *   | 'toast-shown'
+ *   | 'show'
+ *   | 'hide'
+ *   | 'close'
+ *   | 'title'
+ *   | 'html-container'
+ *   | 'actions'
+ *   | 'confirm'
+ *   | 'deny'
+ *   | 'cancel'
+ *   | 'default-outline'
+ *   | 'footer'
+ *   | 'icon'
+ *   | 'icon-content'
+ *   | 'image'
+ *   | 'input'
+ *   | 'file'
+ *   | 'range'
+ *   | 'select'
+ *   | 'radio'
+ *   | 'checkbox'
+ *   | 'label'
+ *   | 'textarea'
+ *   | 'inputerror'
+ *   | 'input-label'
+ *   | 'validation-message'
+ *   | 'progress-steps'
+ *   | 'active-progress-step'
+ *   | 'progress-step'
+ *   | 'progress-step-line'
+ *   | 'loader'
+ *   | 'loading'
+ *   | 'styled'
+ *   | 'top'
+ *   | 'top-start'
+ *   | 'top-end'
+ *   | 'top-left'
+ *   | 'top-right'
+ *   | 'center'
+ *   | 'center-start'
+ *   | 'center-end'
+ *   | 'center-left'
+ *   | 'center-right'
+ *   | 'bottom'
+ *   | 'bottom-start'
+ *   | 'bottom-end'
+ *   | 'bottom-left'
+ *   | 'bottom-right'
+ *   | 'grow-row'
+ *   | 'grow-column'
+ *   | 'grow-fullscreen'
+ *   | 'rtl'
+ *   | 'timer-progress-bar'
+ *   | 'timer-progress-bar-container'
+ *   | 'scrollbar-measure'
+ *   | 'icon-success'
+ *   | 'icon-warning'
+ *   | 'icon-info'
+ *   | 'icon-question'
+ *   | 'icon-error'
+ * } SwalClass
+ * @typedef {Record<SwalClass, string>} SwalClasses
  */
-export const prefix = (items) => {
-  const result = {}
-  for (const i in items) {
-    result[items[i]] = swalPrefix + items[i]
-  }
-  return result
-}
 
-export const swalClasses = prefix([
+/**
+ * @typedef {'success' | 'warning' | 'info' | 'question' | 'error'} SwalIcon
+ * @typedef {Record<SwalIcon, string>} SwalIcons
+ */
+
+/** @type {SwalClass[]} */
+const classNames = [
   'container',
   'shown',
   'height-auto',
@@ -82,6 +151,17 @@ export const swalClasses = prefix([
   'icon-info',
   'icon-question',
   'icon-error',
-])
+]
 
-export const iconTypes = prefix(['success', 'warning', 'info', 'question', 'error'])
+export const swalClasses = classNames.reduce((acc, className) => {
+  acc[className] = swalPrefix + className
+  return acc
+}, /** @type {SwalClasses} */ ({}))
+
+/** @type {SwalIcon[]} */
+const icons = ['success', 'warning', 'info', 'question', 'error']
+
+export const iconTypes = icons.reduce((acc, icon) => {
+  acc[icon] = swalPrefix + icon
+  return acc
+}, /** @type {SwalIcons} */ ({}))

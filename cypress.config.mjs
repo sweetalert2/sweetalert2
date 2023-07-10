7f10d98a0fa38036991cd2d07d1bf575b37c0ddc
swd <73160076+swdmeow@users.noreply.github.com>
2023-07-10 18:15:04 +0500

    Delete cypress.config.mjs

--- cypress.config.mjs
+++
@@ -1,9 +0,0 @@
-import cypress from 'cypress'
-
-export default cypress.defineConfig({
-  e2e: {
-    supportFile: 'cypress/support/index.js',
-    video: false,
-    experimentalRunAllSpecs: true,
-  },
-})

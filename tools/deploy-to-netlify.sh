1225a463545a19373b8b149d113da5e58b64b8c2
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-09-27 00:06:25 +0300

    chore: move PR preview from netlify to render (#2867)

--- tools/deploy-to-netlify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yarn build
-curl https://sweetalert2.github.io -o index.html
-sed -i 's|"//cdn.jsdelivr|"https://cdn.jsdelivr|g' index.html
-sed -i 's|src="/|src="https://sweetalert2.github.io/|g' index.html
-sed -i 's|https://cdn.jsdelivr.net/npm/sweetalert2@11/||g' index.html
-sed -i 's|="./|="https://sweetalert2.github.io/|g' index.html
-sed -i 's|="/|="https://sweetalert2.github.io/|g' index.html

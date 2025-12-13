2e11ae581f99920ca8592e142c41dca85649cde5
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-12-13 13:59:26 +0200

    fix: stop using semantic-release/changelog

--- release.config.js
+++ release.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   debug: true,
   branches: ['main'],
-  verifyConditions: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/github'],
+  verifyConditions: ['@semantic-release/npm', '@semantic-release/github'],
   prepare: [
     {
       path: '@semantic-release/exec',
       cmd: 'VERSION=${nextRelease.version} ./node_modules/.bin/zx tools/build-dist.mjs',
     },
-    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
   ],
